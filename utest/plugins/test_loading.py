97464d24ee1174a00d277fcbbe3c035555261c30
Janne Harkonen <jth@reaktor.fi>
2009-11-06 14:56:32 +0200

    renamed modules

---
+++ utest/plugins/test_loading.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+
+from robotide.plugins.loader import PluginLoader
+from robotide.plugins.releasenotes import ReleaseNotesPlugin
+ReleaseNotesPlugin.auto_show = lambda *args: None
+
+from plugin_resources import FakeApplication
+
+
+class TestablePluginLoader(PluginLoader):
+    def _get_plugin_dirs(self):
+        return [os.path.join(os.path.dirname(__file__), 'plugins_for_loader')]
+
+class TestPluginLoader(unittest.TestCase):
+
+    def test_plugin_loading(self):
+        loader = TestablePluginLoader(FakeApplication())
+        self._assert_plugin_loaded(loader, 'Example Plugin 1')
+        self._assert_plugin_loaded(loader, 'Example Plugin 2')
+        self._assert_plugin_loaded(loader, 'Example Plugin 3')
+        self._assert_plugin_loaded(loader, 'Release Notes')
+
+    def _assert_plugin_loaded(self, loader, name):
+        for p in loader.plugins:
+            if p.name == name:
+                return
+        raise AssertionError("Plugin '%s' not loaded" % name)
+
+
+if __name__ == "__main__":
+    unittest.main()
