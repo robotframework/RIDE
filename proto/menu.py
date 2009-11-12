ee5d9c83500a47ca6f1d2767fbcef913dc0924fa
Janne Harkonen <jth@reaktor.fi>
2009-11-12 15:13:06 +0200

    Proto application

---
+++ proto/menu.py
@@ -0,0 +1,85 @@
+import wx
+
+
+class MenuBar(wx.MenuBar):
+
+    def __init__(self, frame):
+        wx.MenuBar.__init__(self)
+        self._frame = frame
+
+    def register_menu_entry(self, entry):
+        menu = self._get_or_create_menu(entry.menu)
+        entry.register(menu, self._frame)
+        #self._bind_handler(shortcut, action, container, id)
+        #self._menu_items.append((menu, menu_item, action, container))
+        #return id
+
+    def _get_or_create_menu(self, name):
+        position = self.FindMenu(name)
+        if position == -1:
+            self.Append(wx.Menu(), name)
+            position = self.FindMenu(name)
+        return self.GetMenu(position)
+
+
+class MenuEntry(object):
+
+    def __init__(self, menu, name, action, container=None, shortcut=None, doc=''):
+        self.id = wx.NewId()
+        self.menu = menu
+        self.name = self._get_name(name, shortcut)
+        self.doc = doc
+        self.action = ActionFactory(self, action, container)
+
+    def _get_name(self, name, shortcut):
+        if shortcut:
+            return '%s\t%s' % (name, shortcut)
+        return name
+
+    def register(self, menu, frame):
+        menu.Append(self.id, self.name, self.doc)
+        frame.Bind(wx.EVT_MENU, self.action, id=self.id)
+
+
+ACTIONS = {}
+
+def ActionFactory(entry, action, container):
+    proxy = ACTIONS.setdefault((entry.menu, entry.name), ActionProxy())
+    proxy.register_action(Actor(action, container))
+    return proxy
+
+
+class ActionProxy(object):
+
+    def __init__(self):
+        self._actions = []
+
+    def register_action(self, action):
+        self._actions.append(action)
+
+    def __call__(self, event):
+        for actor in self._actions:
+            actor.act()
+
+
+class Actor(object):
+
+    def __init__(self, action, container):
+        self._action = action
+        self._container = container
+
+    def act(self):
+        if self._should_act():
+            self._action()
+
+    def _should_act(self):
+        if self._container is None:
+            return True
+        if not self._container.IsShownOnScreen():
+            return False
+        widget = wx.GetActiveWindow()
+        while widget:
+            if widget == self._container.Parent:
+                return True
+            widget = widget.GetParent()
+        return False
---
+++ proto/protoframe.py
@@ -0,0 +1,74 @@
+import wx
+from wx.lib import flatnotebook as fnb
+
+from menu import *
+
+
+class ProtoFrame(wx.Frame):
+
+    def __init__(self):
+        wx.Frame.__init__(self, None, title='ProtoFrame')
+        self.mb = MenuBar(self)
+        self.mb.register_menu_entry(MenuEntry('File', 'Event', self.OnEvent,
+                                              shortcut='Alt-e'))
+        self.SetMenuBar(self.mb)
+        self.CreateStatusBar()
+        splitter = wx.SplitterWindow(self, style=wx.SP_LIVE_UPDATE)
+        splitter.SetMinimumPaneSize(200)
+        tree = Tree(splitter)
+        self.nb = fnb.FlatNotebook(splitter)
+        self.Bind(fnb.EVT_FLATNOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
+        self.nb.AddPage(ProtoPanel(self, self.nb), 'P1')
+        self.nb.AddPage(AnotherProtoPanel(self, self.nb), 'P2')
+        splitter.SplitVertically(tree, self.nb, 100)
+
+    def OnPageChanging(self, event):
+        newindex = event.GetSelection()
+        if newindex <= self.nb.GetPageCount() - 1:
+            self.nb.GetPage(newindex).SetFocus()
+
+    def register_menu_entry(self, menu, name, action, container=None,
+                            shortcut=None, doc=''):
+        self.mb.register_menu_entry(MenuEntry(menu, name, action, container,
+                                              shortcut, doc))
+
+    def OnEvent(self):
+        wx.MessageBox('Main frame')
+
+
+class Tree(wx.TreeCtrl):
+
+    def __init__(self, parent):
+        wx.TreeCtrl.__init__(self, parent)
+        root = self.AddRoot('root')
+        self.AppendItem(root, 'Foo')
+
+
+class ProtoPanel(wx.Panel):
+
+    def __init__(self, frame, parent):
+        wx.Panel.__init__(self, parent)
+        frame.register_menu_entry('Edit', 'Foo', self.OnMenu, self, 'Ctrl-F',
+                                  'Documentation')
+        wx.TextCtrl(self, value='I AM PANEL 1')
+
+    def OnMenu(self):
+        wx.MessageBox('panel 1')
+
+
+class AnotherProtoPanel(wx.Panel):
+
+    def __init__(self, frame, parent):
+        wx.Panel.__init__(self, parent)
+        frame.register_menu_entry('Edit', 'Foo', self.OnMenu, self, 'Ctrl-F',
+                                  'Some Documentation')
+        wx.TextCtrl(self, value='I AM PANEL 2')
+
+    def OnMenu(self):
+        wx.MessageBox('panel 2')
+
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    ProtoFrame().Show()
+    app.MainLoop()
