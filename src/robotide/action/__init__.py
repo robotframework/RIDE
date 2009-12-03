3c35d0d97a72390532a66336735b57a5fe5dcc8e
Juha Rantanen <juha.rantanen@reaktor.fi>
2009-12-03 13:32:04 +0200

    Added more docs.

--- src/robotide/action/__init__.py
+++ src/robotide/action/__init__.py
@@ -12,5 +12,23 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+
+"""Module for handling UI actions.
+
+Menu entries, keyboard shortcuts and toolbar buttons (UI action) are created 
+using `ActionInfo` class in RIDE. This class is instantiated with proper data 
+and registered to RIDE using register_action method from `Plugin` class.
+Registering mechanism allows multiple actions to be registered to the same
+UI action. In case UI action does not exist, it is created. Menu separators are
+created by instantiating and registering `SeparatorInfo`.
+
+UI action triggers action if the related UI container's child component has
+focus. In case action does not have container, it is always called.
+
+For creating multiple actions there is convenience method `ActionInfoCollection`
+which generates `ActionInfo` objects from given multiline string.
+"""
+
+
 from actioninfo import ActionInfoCollection, SeparatorInfo, ActionInfo
 from action import Action
