73c2b936c68120069722b10628910169ee79e18f
Janne Härkönen <jth@reaktor.fi>
2009-12-29 10:46:18 +0200

    Fixed after attribute rename

--- utest/editor/test_kweditor.py
+++ utest/editor/test_kweditor.py
@@ -3,7 +3,7 @@ import unittest
 from robot.utils.asserts import assert_equals
 
 from robotide.editor.kweditor import KeywordEditor
-from robotide.editor.clipboard import GRID_CLIPBOARD
+from robotide.editor.clipboard import _GRID_CLIPBOARD
 from robotide.publish.messages import RideGridCellChanged
 from robotide.publish import PUBLISHER
 from resources import FakeSuite, PYAPP_REFERENCE #Needed to be able to create wx components
@@ -57,10 +57,10 @@ class TestCoordinates(unittest.TestCase):
         self._verify_selection(0,1,3,4)
 
     def _verify_selection(self, toprow, topcol, botrow, botcol):
-        assert_equals(self._editor._active_coords.topleft.row, toprow)
-        assert_equals(self._editor._active_coords.topleft.col, topcol)
-        assert_equals(self._editor._active_coords.bottomright.row, botrow)
-        assert_equals(self._editor._active_coords.bottomright.col, botcol)
+        assert_equals(self._editor.active_coords.topleft.row, toprow)
+        assert_equals(self._editor.active_coords.topleft.col, topcol)
+        assert_equals(self._editor.active_coords.bottomright.row, botrow)
+        assert_equals(self._editor.active_coords.bottomright.col, botcol)
 
 
 class TestClipBoard(unittest.TestCase):
@@ -80,7 +80,7 @@ class TestClipBoard(unittest.TestCase):
     def _copy_block_and_verify(self, block, exp_content):
         self._editor.SelectBlock(*block)
         self._editor.OnCopy()
-        assert_equals(GRID_CLIPBOARD.get_contents(), exp_content)
+        assert_equals(_GRID_CLIPBOARD.get_contents(), exp_content)
         self._verify_grid_content(DATA)
 
     def test_cut_one_cell(self):
@@ -95,7 +95,7 @@ class TestClipBoard(unittest.TestCase):
 
     def _cut_block_and_verify(self, block, exp_clipboard, exp_grid):
         self._cut_block(block)
-        assert_equals(GRID_CLIPBOARD.get_contents(), exp_clipboard)
+        assert_equals(_GRID_CLIPBOARD.get_contents(), exp_clipboard)
         self._verify_grid_content(exp_grid)
 
     def test_undo_with_cut(self):
