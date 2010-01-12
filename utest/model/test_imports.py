eadcdc161841a17aee674c2a5d6b2b44103ab67f
Janne Harkonen <jth@reaktor.fi>
2010-01-12 07:35:33 +0200

    Whitespace, copyrights

--- utest/model/test_imports.py
+++ utest/model/test_imports.py
@@ -1,3 +1,17 @@
+#  Copyright 2008 Nokia Siemens Networks Oyj
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import unittest
 from robot.utils.asserts import assert_equals
 
@@ -34,7 +48,6 @@ class TestAutomaticHandlingOfFileSeparatorVariable(unittest.TestCase):
 
     def test_variable_is_not_replaced_in_arguments(self):
         assert_equals(self._imports[2].args, ['arg${/}value'])
-
 
 
 if __name__ == '__main__':
