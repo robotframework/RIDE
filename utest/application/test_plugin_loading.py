e3a6889a5408e35ccd950d3765aef26d1d9ff0f6
Janne Harkonen <jth@reaktor.fi>
2010-01-13 14:45:46 +0200

    Cleanup

--- utest/application/test_plugin_loading.py
+++ utest/application/test_plugin_loading.py
@@ -26,10 +26,9 @@ class TestPluginLoader(unittest.TestCase):
     def test_plugin_loading(self):
         plugins_dir = [os.path.join(os.path.dirname(__file__), 'plugins_for_loader')]
         loader = PluginLoader(FakeApplication(), plugins_dir, [Colorizer])
-        self._assert_plugin_loaded(loader, 'Example Plugin 1')
-        self._assert_plugin_loaded(loader, 'Example Plugin 2')
-        self._assert_plugin_loaded(loader, 'Example Plugin 3')
-        self._assert_plugin_loaded(loader, 'Colorizer')
+        for name in ['Example Plugin 1', 'Example Plugin 2',
+                     'Example Plugin 3', 'Colorizer']:
+            self._assert_plugin_loaded(loader, name)
 
     def _assert_plugin_loaded(self, loader, name):
         for p in loader.plugins:
