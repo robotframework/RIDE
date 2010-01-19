ab501e6fce9af6f92af96446d986977945481cb3
Janne Harkonen <jth@reaktor.fi>
2010-01-19 11:34:58 +0200

    whitespace

--- utest/model/test_imports.py
+++ utest/model/test_imports.py
@@ -1,11 +1,11 @@
 #  Copyright 2008 Nokia Siemens Networks Oyj
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ class _ImportItem(object):
 
 
 class TestAutomaticHandlingOfFileSeparatorVariable(unittest.TestCase):
-    """'${/}' should be converted to '/' in import setting names, since RF 
+    """'${/}' should be converted to '/' in import setting names, since RF
     supports the latter both in Windows and Linux.
     """
     def setUp(self):
