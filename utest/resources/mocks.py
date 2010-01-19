81e56cda48df86b9e7026c1c2ccaa6a0277cecea
Janne Harkonen <jth@reaktor.fi>
2010-01-19 10:15:33 +0200

    Correctly determine whether children have already been rendered

--- utest/resources/mocks.py
+++ utest/resources/mocks.py
@@ -1,11 +1,11 @@
 #  Copyright 2008 Nokia Siemens Networks Oyj
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ class MockSerializer(object):
 
     def start_settings(self):
         self.record.append('Start Settings')
-        
+
     def setting(self, name, value):
         self.record.append('Setting: %s | %s' % (name, value))
 
@@ -41,7 +41,7 @@ class MockSerializer(object):
         self.record.append('Start Variables')
 
     def variable(self, name, value):
-        self.record.append('Variable: %s | %s' % (name, value))    
+        self.record.append('Variable: %s | %s' % (name, value))
 
     def end_variables(self):
         self.record.append('End Variables')
@@ -80,6 +80,10 @@ class FakeResource(ResourceFile):
         self.name = name
         self.doc = doc
         self.source = path
+        self.rendered = False
+
+    def _find_source(self, source):
+        return source
 
     def add_uk(self, name, doc=''):
         self.keywords.append(FakeUserKeyword(name, doc))
@@ -94,6 +98,7 @@ class FakeDirectorySuite(InitFile):
         data.initfile = path
         InitFile.__init__(self, data)
         self.name = name
+        self.rendered = False
 
 class FakeSuite(TestCaseFile):
     def __init__(self, name='Fake Suite', doc='', path='fake/suite.html'):
@@ -101,6 +106,7 @@ class FakeSuite(TestCaseFile):
         data.doc = doc
         TestCaseFile.__init__(self, data)
         self.name = name
+        self.rendered = False
 
 class FakeUserKeyword(UserKeyword):
     def __init__(self, datafile, name='Fake UK', doc='Some doc'):
