55ad0ae9361ccd03a42f098eb7f18e375cdda438
Janne Härkönen <jth@reaktor.fi>
2010-01-27 14:53:06 +0200

    unused import

--- src/robotide/run/runanything.py
+++ src/robotide/run/runanything.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import os
 import sys
 import wx
 from wx.lib.mixins.listctrl import TextEditMixin
