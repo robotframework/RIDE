0037727c73afa9d32f45141cd568e7d83910e4cf
Janne Härkönen <jth@reaktor.fi>
2010-01-27 18:23:02 +0200

    whitespace

--- src/robotide/editor/fieldeditors.py
+++ src/robotide/editor/fieldeditors.py
@@ -1,11 +1,11 @@
 #  Copyright 2008-2009 Nokia Siemens Networks Oyj
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -111,7 +111,7 @@ class _EditorGrid(GridEditor):
         self.CreateGrid(rows, cols)
 
     def _initialize_value(self, value):
-        for index, item in enumerate(value): 
+        for index, item in enumerate(value):
             row, col = divmod(index, self.NumberCols)
             self.SetCellValue(row, col, item)
 
