cb1ccfd903a1812e4602bd5a65479c90135450ce
Janne Härkönen <jth@reaktor.fi>
2010-01-27 15:51:55 +0200

    whitespace

--- src/robotide/editor/editordialogs.py
+++ src/robotide/editor/editordialogs.py
@@ -1,11 +1,11 @@
 #  Copyright 2008-2009 Nokia Siemens Networks Oyj
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -89,7 +89,7 @@ class LibraryImportDialog(_Dialog):
 
     def _get_editors(self, item):
         value = item and item.get_str_value() or ''
-        return [ValueEditor(self, value, 
+        return [ValueEditor(self, value,
                             validator=NonEmptyValidator(self._title))]
 
 class VariablesImportDialog(LibraryImportDialog):
