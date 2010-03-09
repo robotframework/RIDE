fde84a0f50112fa998df91428d9896c6b14f574f
Janne Härkönen <jth@reaktor.fi>
2010-03-09 10:38:54 +0200

    cannot use SystemSettings.GetFont before app is created

--- src/robotide/context/__init__.py
+++ src/robotide/context/__init__.py
@@ -49,8 +49,10 @@ SETTING_LABEL_WIDTH = 175
 SETTING_ROW_HEIGTH = 25
 POPUP_BACKGROUND = (255, 255, 187)
 HELP_FONT = (8, wx.FONTFAMILY_DEFAULT, wx.FONTWEIGHT_NORMAL, wx.FONTSTYLE_NORMAL)
-FIXED_WIDTH_FONT = (wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FIXED_FONT).GetPointSize(),
-                    wx.FONTFAMILY_MODERN, FONTWEIGHT_NORMAL, FONTSTYLE_NORMAL)
+def _get_fixed_width_font():
+    return wx.Font(wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FIXED_FONT).GetPointSize(),
+                   wx.FONTFAMILY_MODERN, FONTWEIGHT_NORMAL, FONTSTYLE_NORMAL)
+FIXED_WIDTH_FONT = property(_get_fixed_width_font)
 
 pyversion = '.'.join([ str(v) for v in sys.version_info[:3] ])
 ABOUT_RIDE = '''<h3>RIDE -- Robot Framework Test Data Editor</h3>
