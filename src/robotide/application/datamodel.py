b6b2095cf36fb0029a52c778618cbbe9b5ce3959
Janne Härkönen <jth@reaktor.fi>
2010-03-26 14:58:49 +0200

    unused

--- src/robotide/application/datamodel.py
+++ src/robotide/application/datamodel.py
@@ -12,8 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import operator
-
 from robotide.model import TestSuiteFactory
 from robotide.errors import DataError, SerializationError
 from robotide import context
