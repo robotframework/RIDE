bcc27ae553e07e253c6ffa3594340dbffcb589ca
Janne Härkönen <jth@reaktor.fi>
2010-06-15 07:25:33 +0300

    Fixed attribute access

--- utest/ui/test_tree.py
+++ utest/ui/test_tree.py
@@ -84,7 +84,7 @@ class _BaseSuiteTreeTest(unittest.TestCase):
         res.source = 'resource.txt'
         res.keyword_table.keywords.append(UserKeyword(res, 'Resource Keyword'))
         model = ChiefController(None)
-        model.data = TestDataDirectoryController(suite)
+        model._controller = TestDataDirectoryController(suite)
         model.resources.append(ResourceFileController(res))
         return model
 
