bc2f6a68e01463e00898496296c4911e6985a419
Janne Härkönen <jth@reaktor.fi>
2010-06-23 14:30:07 +0300

    put inside package

--- utest/controller/test_chiefcontroller.py
+++ utest/controller/test_chiefcontroller.py
@@ -16,21 +16,29 @@ class ChiefControllerTest(unittest.TestCase):
         self.ctrl = ChiefController(Namespace())
         self.load_observer = MessageRecordingLoadObserver()
 
-    def test_loading_suite(self):
+    def test_loading_suite_at_startup(self):
         self._load(MINIMAL_SUITE_PATH)
         assert_true(self.ctrl._controller is not None)
 
-    def test_loading_resource(self):
+    def test_loading_resource_at_startup(self):
         self._load(RESOURCE_PATH)
         assert_true(self.ctrl.resources != [])
 
-    def test_loading_invalid_data(self):
+    def test_loading_invalid_data_at_startup(self):
         msg = "Given file 'invalid' is not a valid Robot Framework test case or resource file."
         self._load('invalid')
         assert_equals(self.load_observer.message, msg)
 
     def _load(self, path):
         self.ctrl.load_data(path, self.load_observer)
+        assert_true(self.load_observer.finish_called)
+
+    def test_loading_datafile(self):
+        self.ctrl.load_datafile(MINIMAL_SUITE_PATH, self.load_observer)
+        assert_true(self.load_observer.finish_called)
+
+    def test_loading_resource_file(self):
+        self.ctrl.load_resource(RESOURCE_PATH, self.load_observer)
         assert_true(self.load_observer.finish_called)
 
     def test_loading_invalid_datafile(self):
