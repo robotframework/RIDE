1019b054eac66b206c8131071f9434251beec290
Janne Härkönen <jth@reaktor.fi>
2010-06-23 13:59:39 +0300

    combined test modules

--- utest/controller/test_chiefcontroller.py
+++ utest/controller/test_chiefcontroller.py
@@ -1,18 +1,45 @@
 import unittest
-from robot.utils.asserts import assert_true, assert_equals
+from robot.utils.asserts import assert_true, assert_equals, assert_none
 
 from robotide.controller import ChiefController
 from robotide.namespace import Namespace
 from robotide.controller.filecontroller import TestCaseFileController,\
     TestDataDirectoryController
 
-from resources import COMPLEX_SUITE_PATH, MessageRecordingLoadObserver, SUITEPATH
+from resources import (COMPLEX_SUITE_PATH,  MINIMAL_SUITE_PATH, RESOURCE_PATH,
+                       MessageRecordingLoadObserver, SUITEPATH)
 
 
 class ChiefControllerTest(unittest.TestCase):
 
     def setUp(self):
         self.ctrl = ChiefController(Namespace())
+        self.load_observer = MessageRecordingLoadObserver()
+
+    def test_loading_suite(self):
+        self._load(MINIMAL_SUITE_PATH)
+        assert_true(self.ctrl._controller is not None)
+
+    def test_loading_resource(self):
+        self._load(RESOURCE_PATH)
+        assert_true(self.ctrl.resources != [])
+
+    def test_loading_invalid_data(self):
+        msg = "Given file 'invalid' is not a valid Robot Framework test case or resource file."
+        self._load('invalid')
+        assert_equals(self.load_observer.message, msg)
+
+    def _load(self, path):
+        self.ctrl.load_data(path, self.load_observer)
+        assert_true(self.load_observer.finish_called)
+
+    def test_loading_invalid_datafile(self):
+        self.ctrl.load_datafile('invalid', self.load_observer)
+        assert_equals(self.load_observer.message, "Invalid data file 'invalid'.")
+
+    def test_loading_invalid_resource(self):
+        assert_none(self.ctrl.load_resource('invalid', self.load_observer))
+        assert_equals(self.load_observer.message, "Invalid resource file 'invalid'.")
 
     def test_dirtyness(self):
         self.ctrl.load_data(COMPLEX_SUITE_PATH, MessageRecordingLoadObserver())
@@ -39,7 +66,7 @@ class ChiefControllerTest(unittest.TestCase):
                 return child
         return None
 
-    def test_resources_opening(self):
+    def test_creating_new_resource(self):
         controller = self.ctrl.new_resource('somepath')
         assert_equals(controller.name, 'Somepath')
 
