671cf98469b9c143b5164573694af59225c13cd5
Janne Härkönen <jth@reaktor.fi>
2010-06-29 15:14:34 +0300

    added fixme

--- src/robotide/editor/dialoghelps.py
+++ src/robotide/editor/dialoghelps.py
@@ -33,6 +33,7 @@ List Variable
 Give name and value of the variable. Input list variable items into separate cells.
 
 Library
+FIXME: arg editing has changed
 Give name of the library to import.
 Separate possible arguments with a pipe character like 'MyLibrary | arg 1 | arg 2'.
 %(ESCAPE)s
