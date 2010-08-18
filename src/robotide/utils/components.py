b22766a7153c7c66c4517562d5b03887b03ffe6c
Janne Härkönen <jth@reaktor.fi>
2010-08-18 13:57:46 +0300

    Notify parent of key events

--- src/robotide/utils/components.py
+++ src/robotide/utils/components.py
@@ -1,11 +1,11 @@
 #  Copyright 2008-2009 Nokia Siemens Networks Oyj
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,11 @@ class RideHtmlWindow(HtmlWindow):
         self.SetStandardFonts()
         if text:
             self.SetPage(text)
+        self.Bind(wx.EVT_KEY_DOWN, self.OnKey)
+
+    def OnKey(self, event):
+        self.Parent.OnKey(event)
+        event.Skip()
 
     def OnLinkClicked(self, link):
         webbrowser.open(link.Href)
