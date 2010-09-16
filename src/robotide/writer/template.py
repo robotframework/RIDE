810ee18f90763eb93231de8d8c12d5b3126051c0
Janne Härkönen <jth@reaktor.fi>
2010-09-16 10:10:48 +0300

    Try to determine encoding of html file when template is read

--- src/robotide/writer/template.py
+++ src/robotide/writer/template.py
@@ -15,7 +15,6 @@
 import os
 import re
 
-from robotide import utils
 import htmltemplate
 
 
@@ -33,6 +32,8 @@ content=["\']?true["\']?\s*
 ){2}/?>          # allows also name or content twice, but that's ok
 .*</head>
 ''', re.IGNORECASE | re.DOTALL | re.VERBOSE)
+charset_re = re.compile('<meta.* content="text/html; charset=(.*)"', re.IGNORECASE)
+
 
 
 def Template(data_path, name):
@@ -43,10 +44,14 @@ def Template(data_path, name):
         tmpl = htmltemplate.TEMPLATE
     return tmpl % {'NAME': name}
 
+
+
 def _get_template(data_file, default_template, resource_file=False):
     content = []
     template_matches = False
+    encoding = None
     for line in data_file.readlines():
+        line, encoding = _process_line(line, encoding)
         content.append(line)
         if not template_matches and '</head>' in line.lower():
             if _meta_re.search(''.join(content)):
@@ -57,6 +62,15 @@ def _get_template(data_file, default_template, resource_file=False):
     if resource_file:
         tmpl = _testcases_re.sub('', tmpl)
     return tmpl
+
+def _process_line(line, encoding):
+    if encoding:
+        return unicode(line, encoding), encoding
+    return line, _parse_encoding(line)
+
+def _parse_encoding(line):
+    match = charset_re.match(line)
+    return match.group(1) if match else None
 
 def settings_table(table, content):
     return _table(_settings_re, table, content)
@@ -71,7 +85,8 @@ def keywords_table(table, content):
     return _table(_keywords_re, table, content)
 
 def _table(table_re, table, content):
-    return table_re.sub(_table_replacer(table), content)
+    replaced = _table_replacer(table)
+    return table_re.sub(replaced, content)
 
 def _table_replacer(content):
     content = content.strip()
