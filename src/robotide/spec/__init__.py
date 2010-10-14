34fadc384c2aeb575389510f9669be6e99f7af7d
Juha Rantanen <devnull@localhost>
2010-10-14 15:47:05 +0300

    Removed dead code.

--- src/robotide/spec/__init__.py
+++ src/robotide/spec/__init__.py
@@ -12,5 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from xmlreaders import XMLResource, LibrarySpec, _XMLResource
+from xmlreaders import LibrarySpec
 
