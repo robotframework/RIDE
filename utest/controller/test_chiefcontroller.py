72f33f23a9836ef2c4be6da1c1f3e73d7b0b8a4f
Juha Rantanen <juha.rantanen@reaktor.fi>
2010-10-19 13:06:45 +0300

    Added more test to verify that RideOpenSuite and RideOpenResource messages are published correctly, issue 554.

--- utest/controller/test_chiefcontroller.py
+++ utest/controller/test_chiefcontroller.py
@@ -1,34 +1,70 @@
 import unittest
 from robot.utils.asserts import assert_true, assert_equals, assert_none
 
+
 from robotide.controller import ChiefController
 from robotide.namespace import Namespace
-from robotide.controller.filecontroller import TestCaseFileController,\
+from robotide.controller.filecontroller import TestCaseFileController, \
     TestDataDirectoryController
+from robotide.publish import PUBLISHER
+from robotide.publish.messages import RideOpenSuite, RideOpenResource
 
 from resources import (COMPLEX_SUITE_PATH, MINIMAL_SUITE_PATH, RESOURCE_PATH,
                        MessageRecordingLoadObserver, SUITEPATH)
 
+
+class NoneData(object):
+    path = None
+
+class PublisherListener(object):
+
+    def __init__(self, topic):
+        PUBLISHER.subscribe(self._listener, topic, self)
+        self._topic = topic
+        self.data = NoneData()
+        self.count = 0
+
+    def _listener(self, data):
+        self.data = data
+        self.count += 1
+
+    def unsuscribe(self):
+        PUBLISHER.unsubscribe(self._listener, self._topic, self)
 
 class ChiefControllerTest(unittest.TestCase):
 
     def setUp(self):
         self.ctrl = ChiefController(Namespace())
         self.load_observer = MessageRecordingLoadObserver()
+        self.suite_listener = PublisherListener(RideOpenSuite)
+        self.resource_listener = PublisherListener(RideOpenResource)
+
+    def tearDown(self):
+        self.suite_listener.unsuscribe()
+        self.resource_listener.unsuscribe()
 
     def test_loading_suite_at_startup(self):
         self._load(MINIMAL_SUITE_PATH)
         assert_true(self.ctrl._controller is not None)
+        self._test_listeners(MINIMAL_SUITE_PATH, None)
+
+    def _test_listeners(self, suite_path, resource_path):
+        assert_equals(self.suite_listener.data.path, suite_path)
+        assert_equals(self.suite_listener.count, 1 if suite_path else 0)
+        assert_equals(self.resource_listener.data.path, resource_path)
+        assert_equals(self.resource_listener.count, 1 if resource_path else 0)
 
     def test_loading_resource_at_startup(self):
         self._load(RESOURCE_PATH)
         assert_true(self.ctrl.resources != [])
+        self._test_listeners(None, RESOURCE_PATH)
 
     def test_loading_invalid_data_at_startup(self):
         msg = "Given file 'invalid' is not a valid Robot Framework test case or resource file."
         self.ctrl.load_data('invalid', self.load_observer)
         assert_true(self.load_observer.finished)
         assert_equals(self.load_observer.message, msg)
+        self._test_listeners(None, None)
 
     def _load(self, path):
         self.ctrl.load_data(path, self.load_observer)
@@ -36,20 +72,26 @@ class ChiefControllerTest(unittest.TestCase):
         assert_true(self.load_observer.finished)
 
     def test_loading_datafile(self):
-        self.ctrl.load_datafile(MINIMAL_SUITE_PATH, self.load_observer)
+        data = self.ctrl.load_datafile(MINIMAL_SUITE_PATH, self.load_observer)
         assert_true(self.load_observer.finished)
+        assert_true(data is not None)
+        self._test_listeners(MINIMAL_SUITE_PATH, None)
 
     def test_loading_resource_file(self):
-        self.ctrl.load_resource(RESOURCE_PATH, self.load_observer)
+        resource = self.ctrl.load_resource(RESOURCE_PATH, self.load_observer)
         assert_true(self.load_observer.finished)
+        assert_true(resource is not None)
+        self._test_listeners(None, RESOURCE_PATH)
 
     def test_loading_invalid_datafile(self):
         self.ctrl.load_datafile('invalid', self.load_observer)
         assert_equals(self.load_observer.message, "Invalid data file 'invalid'.")
+        self._test_listeners(None, None)
 
     def test_loading_invalid_resource(self):
         assert_none(self.ctrl.load_resource('invalid', self.load_observer))
         assert_equals(self.load_observer.message, "Invalid resource file 'invalid'.")
+        self._test_listeners(None, None)
 
     def test_dirtyness(self):
         self.ctrl.load_data(COMPLEX_SUITE_PATH, MessageRecordingLoadObserver())
