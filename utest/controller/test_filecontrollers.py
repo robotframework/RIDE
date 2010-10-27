cd04facefb5951e4db4e5056294f532bb97c3f2b
Robot Framework Developers (robotframework@gmail.com) <Robot Framework Developers (robotframework@gmail.com)>
2010-10-27 09:28:32 +0300

    Refactor TestCaseFileControllerTest

--- utest/controller/test_filecontrollers.py
+++ utest/controller/test_filecontrollers.py
@@ -14,72 +14,70 @@ from resources import SUITEPATH
 
 class TestCaseFileControllerTest(unittest.TestCase):
 
+    SOURCE_HTML = '/tmp/.path.with.dots/test.cases.html'
+    SOURCE_TXT = SOURCE_HTML.replace('.html', '.txt')
+
+    def setUp(self):
+        self.ctrl = TestCaseFileController(TestCaseFile())
+        self.ctrl.data.source = self.SOURCE_HTML
+
     def test_creation(self):
-        ctrl = self._file_ctrl()
-        for st in ctrl.settings:
+        for st in self.ctrl.settings:
             assert_true(st is not None)
-        assert_equals(len(ctrl.settings), 9)
-        assert_false(ctrl.dirty)
+        assert_equals(len(self.ctrl.settings), 9)
+        assert_false(self.ctrl.dirty)
 
     def test_has_format(self):
-        ctrl = self._file_ctrl()
-        assert_true(ctrl.has_format())
-        ctrl.data.source = '/tmp/.path.with.dots/test.cases.html'
-        assert_true(ctrl.has_format())
+        assert_true(self.ctrl.has_format())
 
     def test_get_format(self):
-        ctrl = self._file_ctrl()
-        ctrl.data.source = '/tmp/.path.with.dots/test.cases.html'
-        assert_equals(ctrl.get_format(), 'html')
+        assert_equals(self.ctrl.get_format(), 'html')
+
+    def test_source(self):
+        assert_equals(self.ctrl.source, self.SOURCE_HTML)
 
     def test_set_format(self):
-        ctrl = self._file_ctrl()
-        ctrl.data.source = '/tmp/.path.with.dots/test.cases.html'
-        assert_equals(ctrl.source, '/tmp/.path.with.dots/test.cases.html')
-        ctrl.set_format('txt')
-        assert_equals(ctrl.source, '/tmp/.path.with.dots/test.cases.txt')
+        self.ctrl.set_format('txt')
+        assert_equals(self.ctrl.source, self.SOURCE_TXT)
 
     def test_add_test_or_kw(self):
-        ctrl = self._file_ctrl()
-        assert_equals(len(ctrl.tests), 0)
-        new_test = TestCaseController(ctrl, TestCase(TestCaseFile(), 'New test'))
-        ctrl.add_test_or_keyword(new_test)
-        assert_equals(len(ctrl.tests), 1)
-        assert_true(ctrl.tests[0]._test.parent is ctrl.datafile)
-        assert_true(ctrl.dirty)
+        assert_equals(len(self.ctrl.tests), 0)
+        new_test = TestCaseController(self.ctrl, TestCase(TestCaseFile(), 'New test'))
+        self.ctrl.add_test_or_keyword(new_test)
+        assert_equals(len(self.ctrl.tests), 1)
+        assert_true(self.ctrl.tests[0]._test.parent is self.ctrl.datafile)
+        assert_true(self.ctrl.dirty)
 
     def test_new_test(self):
-        test_ctrl = self._file_ctrl().create_test('Foo')
+        test_ctrl = self.ctrl.create_test('Foo')
         assert_equals(test_ctrl.name, 'Foo')
 
     def test_create_keyword(self):
-        kw_ctrl = self._file_ctrl().create_keyword('An UK')
+        kw_ctrl = self.ctrl.create_keyword('An UK')
         assert_equals(kw_ctrl.name, 'An UK')
 
     def test_create_keyword_with_args(self):
-        kw_ctrl = self._file_ctrl().create_keyword('UK', '${a1} | ${a2}')
+        kw_ctrl = self.ctrl.create_keyword('UK', '${a1} | ${a2}')
         assert_equals(kw_ctrl.name, 'UK')
         assert_equals(kw_ctrl.data.args.value, ['${a1}', '${a2}'])
-
-    def _file_ctrl(self):
-        return TestCaseFileController(TestCaseFile())
 
 
 class TestDataDirectoryControllerTest(unittest.TestCase):
 
+    def setUp(self):
+        self.ctrl = TestDataDirectoryController(TestDataDirectory())
+
     def test_creation(self):
-        ctrl = TestDataDirectoryController(TestDataDirectory())
-        for st in ctrl.settings:
+        for st in self.ctrl.settings:
             assert_true(st is not None)
-        assert_equals(len(ctrl.settings), 6)
+        assert_equals(len(self.ctrl.settings), 6)
 
     def test_has_format(self):
-        ctrl = TestDataDirectoryController(TestDataDirectory())
-        assert_false(ctrl.has_format())
-        ctrl.mark_dirty()
-        assert_false(ctrl.has_format())
-        ctrl.data.initfile = '/tmp/__init__.html'
-        assert_true(ctrl.has_format())
+        assert_false(self.ctrl.has_format())
+        self.ctrl.mark_dirty()
+        assert_false(self.ctrl.has_format())
+        self.ctrl.data.initfile = '/tmp/__init__.html'
+        assert_true(self.ctrl.has_format())
 
     def test_set_format(self):
         dir = TestDataDirectory()
@@ -91,8 +89,7 @@ class TestDataDirectoryControllerTest(unittest.TestCase):
         assert_equals(ctrl.source, '/tmp/__init__.txt')
 
     def test_adding_new_child(self):
-        ctrl = TestDataDirectoryController(TestDataDirectory())
-        assert_true(ctrl.new_datafile(NewDatafile('path/to/data.txt',
+        assert_true(self.ctrl.new_datafile(NewDatafile('path/to/data.txt',
                                                   is_dir_type=False)))
 
 
