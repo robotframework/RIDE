52980d18b9eb83eaef774aef265a1c50fefcb686
Mikko Korpela <mikko.korpela@gmail.com>
2010-11-11 12:26:23 +0200

    test_ns_performance: Add performance test that fails on O((number of user keywords)^2) or greater growth in results

--- utest/namespace/test_ns_performance.py
+++ utest/namespace/test_ns_performance.py
@@ -6,25 +6,80 @@ from robotide.namespace import Namespace
 from robotide.controller.chiefcontroller import ChiefController
 
 from resources import MessageRecordingLoadObserver
-from datafilereader import TESTCASEFILE_WITH_EVERYTHING
+from datafilereader import TESTCASEFILE_WITH_EVERYTHING, KW1000_TESTCASEFILE,\
+    KW2000_TESTCASEFILE, KW3000_TESTCASEFILE, KW4000_TESTCASEFILE
 
 class TestNamespacePerformance(unittest.TestCase):
 
+    SAFETY_MARGIN = 0.96
+    RELEVANT_B_RELATIVE_TO_C = 0.2
+
     def test_user_keyword_find_performance(self):
-        self._execute_keyword_find_function_n_times('is_user_keyword', 5000)
+        times = 5000
+        end_time = self._execute_keyword_find_function_n_times('is_user_keyword', times)
+        assert_true(end_time < 0.5, 'Checking %d kws took too long: %fs.' % (times, end_time))
 
     def test_library_keyword_find_performance(self):
-        self._execute_keyword_find_function_n_times('is_library_keyword', 5000)
+        times = 5000
+        end_time = self._execute_keyword_find_function_n_times('is_library_keyword', times)
+        assert_true(end_time < 0.5, 'Checking %d kws took too long: %fs.' % (times, end_time))
 
-    def _execute_keyword_find_function_n_times(self, function, n):
+    def test_measure_user_keyword_find_performance(self):
+        times = 1000
+        kw1000_result = self._execute_keyword_find_function_n_times('is_user_keyword', times, KW1000_TESTCASEFILE)
+        kw2000_result = self._execute_keyword_find_function_n_times('is_user_keyword', times, KW2000_TESTCASEFILE)
+        kw3000_result = self._execute_keyword_find_function_n_times('is_user_keyword', times, KW3000_TESTCASEFILE)
+        a, b, c = self._calculate_power2_estimate_constants(kw1000_result, kw2000_result, kw3000_result)
+        assert_true(b > c or (c <= 0),
+                    'Possibly o(n*2) or greater growth in user keyword performance measures!\nkw1000 time = %s kw2000 time = %s kw3000 time = %s'\
+                     % (kw1000_result, kw2000_result, kw3000_result))
+        kw4000_result = self._execute_keyword_find_function_n_times('is_user_keyword', times, KW4000_TESTCASEFILE)
+        self._verify_that_power2_estimate_overestimates(a, b, c, kw1000_result, kw2000_result, kw3000_result, kw4000_result)
+
+    def _verify_that_power2_estimate_overestimates(self, a, b, c, kw1000_result, kw2000_result, kw3000_result, kw4000_result):
+        def power2estimate(kw_amount):
+            x = kw_amount / 1000
+            return a + b * x + c * x**2
+        if c > 0 and (b <= 0 or c / b > self.RELEVANT_B_RELATIVE_TO_C):
+            assert_true(power2estimate(4000) * self.SAFETY_MARGIN > kw4000_result,
+                        'Possibly o(n*2) or greater growth in namespace performance measures!\nkw1000 time = %s kw2000 time = %s kw3000 time = %s kw4000 time = %s'\
+                        % (kw1000_result, kw2000_result, kw3000_result, kw4000_result))
+
+
+    def _calculate_power2_estimate_constants(self, kw1000_result, kw2000_result, kw3000_result):
+        # Assume
+        # a + b * 1000kw_amount + c * 1000kw_amount**2
+        # THE MATH
+        # 1 1 1 [a] = kw1000_result
+        # 1 2 4 [b] = kw2000_result
+        # 1 3 9 [c] = kw3000_result
+        # -- reduce [a]
+        # 0 1 3 [b] = kw2000_result-kw1000_result
+        # 0 2 8 [c] = kw3000_result-kw1000_result
+        # -- reduce [b]
+        # 0 0 1 [c] = (kw3000_result-kw1000_result-2*(kw2000_result-kw1000_result))/2
+        # -- reduce [c]
+        # 1 1 0 [a] = kw1000_result - c
+        # 0 1 0 [b] = kw2000_result-kw1000_result - 3*c
+        # -- reduce [b]
+        # a = kw1000_result - c - b
+        c = (kw3000_result-kw1000_result-2*(kw2000_result-kw1000_result))/2
+        b = kw2000_result-kw1000_result - 3*c
+        a = kw1000_result - c - b
+        return a, b, c
+
+    def _load(self, testcasefile):
         ns = Namespace()
-        chief = ChiefController(ns)
-        chief.load_datafile(TESTCASEFILE_WITH_EVERYTHING,
+        chief =  ChiefController(ns)
+        chief.load_datafile(testcasefile,
                             MessageRecordingLoadObserver())
-        everything_tcf = chief._controller.data
+        return ns, chief._controller.data
+
+    def _execute_keyword_find_function_n_times(self, function, n, filename=TESTCASEFILE_WITH_EVERYTHING):
+        ns, testcasefile = self._load(filename)
+        func = getattr(ns, function)
         start_time = time.time()
         for i in range(n):
-            func = getattr(ns, function)
-            func(everything_tcf, 'hevonen %s' % i)
-        end_time = time.time() - start_time
-        assert_true(end_time < 0.5, 'Checking %d kws took too long: %fs.' % (n, end_time))
+            func(testcasefile, 'hevonen %s' % i)
+        return time.time() - start_time
+        
