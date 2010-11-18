df05088b5a6233cb262017b8489723c23000eb17
Robot Framework Developers (robotframework@gmail.com) <Robot Framework Developers (robotframework@gmail.com)>
2010-11-18 13:55:58 +0200

    images: Add variable

--- src/robotide/ui/images.py
+++ src/robotide/ui/images.py
@@ -14,6 +14,7 @@
 
 import os
 import wx
+from robot.parsing.model import Variable
 
 
 _SIZE = (16, 16)
@@ -29,7 +30,8 @@ class TreeImageList(wx.ImageList):
             'TestCaseFile': _TreeImage(self, wx.ART_NORMAL_FILE),
             'TestCase': _TreeImage(self, 'robot.png'),
             'UserKeyword': _TreeImage(self, 'process.png'),
-            'ResourceFile': _TreeImage(self, wx.ART_NORMAL_FILE)
+            'ResourceFile': _TreeImage(self, wx.ART_NORMAL_FILE),
+            'Variable': _TreeImage(self, 'process.png')
         }
 
     def __getitem__(self, key):
