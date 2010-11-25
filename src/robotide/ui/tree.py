c0786921a3bf45e12df409839fdc6554b12b3607
Janne Härkönen <jth@reaktor.fi>
2010-11-25 09:57:06 +0200

    tree: added public method for selecting node based on controller

--- src/robotide/ui/tree.py
+++ src/robotide/ui/tree.py
@@ -292,14 +292,16 @@ class Tree(treemixin.DragAndDrop, wx.TreeCtrl, utils.RideEventHandler):
             if text.startswith('*') and not handler.controller.dirty:
                 self.SetItemText(node, text[1:])
 
-    def select_tree_item(self, tcuk):
-        '''Find and select the tree item associated with a test case or user keyword'''
-        parent_node = self._get_datafile_node(tcuk.datafile)
+    def select_tree_item_by_data(self, controller):
+        '''Find and select the tree item associated with the given controller.
+
+        Controller can be any of the controllers that are represented in the tree.'''
+        parent_node = self._get_datafile_node(controller.datafile)
         if not parent_node:
             return None
         if not self.IsExpanded(parent_node):
             self._expand_and_render_children(parent_node)
-        node = self._get_node_with_label(parent_node, utils.normalize(tcuk.name))
+        node = self._find_node_by_controller(controller)
         if node != self.GetSelection():
             self.SelectItem(node)
         return node
