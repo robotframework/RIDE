f454b5110411f68d04f29df0f78a9578921ce5a5
Janne Härkönen <jth@reaktor.fi>
2010-12-16 13:52:42 +0200

    Fixed tests AFTER implementation changes

--- utest/spec/test_iteminfo.py
+++ utest/spec/test_iteminfo.py
@@ -11,8 +11,14 @@ from robotide.spec.iteminfo import LibraryKeywordInfo, TestCaseUserKeywordInfo, 
 testlibpath = os.path.join(os.path.dirname(__file__), '..', 'resources', 'robotdata', 'libs')
 sys.path.append(testlibpath)
 
+
 class _FakeTestCaseFile(object):
     source = '/path/to/testcase.txt'
+
+def assert_in_details(kw_info, *expecteds):
+    details = kw_info.details
+    for e in expecteds:
+        assert_true(e in details, details)
 
 
 class TestKeywordInfo(unittest.TestCase):
@@ -21,15 +27,16 @@ class TestKeywordInfo(unittest.TestCase):
         libname = 'TestLib'
         lib = TestLibrary(libname)
         kw_info = LibraryKeywordInfo(lib.handlers['testlib_keyword_with_args'])
-        exp_start = 'Source: TestLib &lt;test library&gt;<br><br>Arguments: [ arg1 | arg2=default value | *args ]<br><br>'
-        assert_true(kw_info.details.startswith(exp_start), kw_info.details)
+        assert_in_details(kw_info, 'Testlib',
+                          '[ arg1 | arg2=default value | *args ]')
 
     def test_uk_arguments_parsing(self):
         uk = UserKeyword(_FakeTestCaseFile(), 'My User keyword')
         uk.args.value = ['${arg1}', '${arg2}=def', '@{varargs}']
         kw_info = TestCaseUserKeywordInfo(uk)
-        exp_start = 'Source: testcase.txt &lt;test case file&gt;<br><br>Arguments: [ arg1 | arg2=def | *varargs ]<br><br>'
-        assert_true(kw_info.details.startswith(exp_start), kw_info.details)
+        exp_source = 'testcase.txt'
+        exp_args = '[ arg1 | arg2=def | *varargs ]'
+        assert_in_details(kw_info, exp_source, exp_args)
 
 
 class TestVariableInfo(unittest.TestCase):
@@ -40,7 +47,7 @@ class TestVariableInfo(unittest.TestCase):
         value = True
         info = VariableInfo(name, value, source)
         assert_equals(info.name, name)
-        assert_equals(info.details, 'Source: %s<br><br>Value:<br>%s' % (source, value))
+        assert_in_details(info, source, 'True')
 
     def test_variable_item_info_when_value_none(self):
         name = '${foo}'
@@ -48,7 +55,7 @@ class TestVariableInfo(unittest.TestCase):
         value = None
         info = VariableInfo(name, value, source)
         assert_equals(info.name, name)
-        assert_equals(info.details, 'Source: %s<br><br>Value:<br>%s' % (source, value))
+        assert_in_details(info, source, '')
 
     def test_list_variable_item_info(self):
         name = '@{foo}'
@@ -56,15 +63,15 @@ class TestVariableInfo(unittest.TestCase):
         value = [1,2,3]
         info = VariableInfo(name, value, source)
         assert_equals(info.name, name)
-        assert_equals(info.details, 'Source: %s<br><br>Value:<br>[ %s ]' % (source, ' | '.join(str(i) for i in value)))
-    
+        assert_in_details(info, source, '[ 1 | 2 | 3 ]')
+
     def test_list_variable_item_info_when_value_none(self):
         name = '@{foo}'
         source = 'source'
         value = None
         info = VariableInfo(name, value, source)
         assert_equals(info.name, name)
-        assert_equals(info.details, 'Source: %s<br><br>Value:<br>[  ]' % source)
+        assert_in_details(info, source, '')
 
 if __name__ == "__main__":
     unittest.main()
