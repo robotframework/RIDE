147782151ffce7eaa3b1fa8c1046761fa6a8c734
Janne Härkönen <jth@reaktor.fi>
2010-12-17 13:12:23 +0200

    Default button now has borders

--- src/robotide/widgets/button.py
+++ src/robotide/widgets/button.py
@@ -4,8 +4,8 @@ import wx
 class ButtonWithHandler(wx.Button):
 
     def __init__(self, parent, label, handler=None, width=-1,
-                 height=25, style=wx.NO_BORDER):
-        wx.Button.__init__(self, parent, style=style, label=label,
+                 height=25):
+        wx.Button.__init__(self, parent, label=label,
                            size=(width, height))
         if not handler:
             handler = getattr(parent, 'On'+label.replace(' ', ''))
