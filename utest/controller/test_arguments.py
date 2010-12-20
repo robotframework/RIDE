f15791cc985dec7186c7f5b211385dc855b8726a
Juha Rantanen <juha.rantanen@reaktor.fi>
2010-12-20 15:15:14 +0200

    Added embedded keywords variables to variable completition.

--- utest/controller/test_arguments.py
+++ utest/controller/test_arguments.py
@@ -8,11 +8,31 @@ class TestParseArguments(unittest.TestCase):
 
     def test_parse(self):
         args = ['${my arg}', '${default}=huhuu', '@{list}', 'invalid${}{}{{']
-        parsed = parse_arguments_to_var_dict(args)
+        parsed = parse_arguments_to_var_dict(args, 'Keyword name')
         assert_equals(parsed['${my arg}'], None)
         assert_equals(parsed['${default}'], 'huhuu')
         assert_equals(parsed['@{list}'], None)
         assert_equals(len(parsed.keys()), 3)
+
+    def test_parse_with_no_args(self):
+        parsed = parse_arguments_to_var_dict([], 'Keyword name')
+        assert_equals(len(parsed.keys()), 0)
+ 
+    def test_embedded_arguments(self):
+        parsed = parse_arguments_to_var_dict([], "Here is ${arg} and ${another arg}")
+        assert_equals(parsed['${arg}'], None)
+        assert_equals(parsed['${another arg}'], None)
+        assert_equals(len(parsed.keys()), 2)
+
+    def test_embedded_arguments_with_list_var_syntax(self):
+        parsed = parse_arguments_to_var_dict([], "Here is ${arg} and @{list arg}")
+        assert_equals(parsed['${arg}'], None)
+        assert_equals(len(parsed.keys()), 1)
+
+    def test_embedded_arguments_with_args(self):
+        parsed = parse_arguments_to_var_dict(['${my arg}'], "Here is ${arg} and ${another arg}")
+        assert_equals(parsed['${my arg}'], None)
+        assert_equals(len(parsed.keys()), 1)
 
 
 class TestArgument(unittest.TestCase):
