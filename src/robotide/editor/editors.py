ff48d6af2628d11182780727e468c4dbc3508780
Juha Rantanen <juha.rantanen@reaktor.fi>
2011-01-17 17:30:24 +0200

    Returned Edit button as editing is not possible without it in OSX. Fixed issue 662.

--- src/robotide/editor/editors.py
+++ src/robotide/editor/editors.py
@@ -239,7 +239,8 @@ class SettingEditor(wx.Panel, RideEventHandler):
         self.update_value()
         self._tooltip = self._get_tooltip()
         sizer.Add(self._value_display, 1, wx.EXPAND)
-        sizer.Add(ButtonWithHandler(self, 'Clear'), 0, wx.RIGHT, 5)
+        sizer.Add(ButtonWithHandler(self, 'Edit'), flag=wx.LEFT|wx.RIGHT, border=5)
+        sizer.Add(ButtonWithHandler(self, 'Clear'))
         sizer.Layout()
         self.SetSizer(sizer)
 
