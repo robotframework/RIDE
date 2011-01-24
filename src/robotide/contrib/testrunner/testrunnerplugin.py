8b51b3445a730f5ab3d08634ce4ea6f99cd3f3dd
Juha Rantanen <juha.rantanen@reaktor.fi>
2011-01-24 14:51:50 +0200

    Changed plugin to use settings through Plugin API. Refactored related code.

--- src/robotide/contrib/testrunner/testrunnerplugin.py
+++ src/robotide/contrib/testrunner/testrunnerplugin.py
@@ -41,7 +41,6 @@ import shutil
 import signal
 from robot.parsing.model import TestCase
 from robotide.pluginapi import Plugin
-from robotide.context import SETTINGS
 from robotide.publish import (RideUserKeywordAdded, RideTestCaseAdded,
                               RideOpenSuite, RideOpenResource,
                               RideNotebookTabChanged, RideItemNameChanged,
@@ -83,9 +82,19 @@ def _RunProfile(name, run_prefix):
 
 class TestRunnerPlugin(Plugin):
     """A plugin for running tests from within RIDE"""
+    defaults = { "auto_save": False,
+                "profile": "pybot",
+                "include_tags": "",
+                "exclude_tags": "",
+                "apply_include_tags": False,
+                "apply_exclude_tags": False,
+                "port": 5010,
+                "sash_position": 200,
+                "runprofiles": [('jybot', 'jybot' + ('.bat' if os.name == 'nt' else ''))]}
 
     def __init__(self, application=None):
-        Plugin.__init__(self, application, initially_enabled=True)
+        Plugin.__init__(self, application, initially_enabled=True,
+                        default_settings=self.defaults)
         self.version = "3.0"
         self.metadata = {"url": "http://code.google.com/p/robotframework-ride/wiki/TestRunnerPlugin"}
 
@@ -96,17 +105,6 @@ class TestRunnerPlugin(Plugin):
         self._report_file = None
         self._log_file = None
         self.profiles = {}
-        self.settings = SETTINGS.add_section("com.orbitz.testrunner")
-        self.settings.set_defaults(auto_save = False, 
-                                   profile = "Service Testing",
-                                   include_tags = "",
-                                   exclude_tags = "",
-                                   apply_include_tags = True,
-                                   apply_exclude_tags = True,
-                                   port = 5010,
-                                   sash = 200,
-                                   runprofiles = [('jybot', 'jybot' + ('.bat' if os.name == 'nt' else ''))]
-                                   )
         self._controls = {}
         self._save_timer = None
         self._server = None
@@ -116,21 +114,28 @@ class TestRunnerPlugin(Plugin):
         self._output_dir = "."
 
     def enable(self):
-        '''Enable the plugin'''
+        self._read_run_profiles()
+        self._build_ui()
+        self.SetProfile(self.profile)
+        self._subscribe_to_events()
+        self._start_listener_server()
+        self._load_tree_if_data_is_open()
+        self._create_temporary_directory()
+
+    def _read_run_profiles(self):
         self._read_run_profiles_from_config()
+        self._read_run_profiles_from_classes()
+
+    def _read_run_profiles_from_config(self):
+        #Have to keep reference so that these classes are not garbage collected
+        self._profile_classes_from_config = [_RunProfile(name, run_prefix)
+                                             for name, run_prefix in self.runprofiles]
+
+    def _read_run_profiles_from_classes(self):
         for profile in runprofiles.BaseProfile.__subclasses__():
             self.profiles[profile.name] = profile(plugin=self)
 
-        self._build_ui()
-
-        if self.settings["profile"] in self.profiles:
-            self.SetProfile(self.settings["profile"])
-        else:
-            # "should never happen"
-            default = sorted(self.profiles.keys())[0]
-            self.SetProfile(default)
-
-        self.active = True
+    def _subscribe_to_events(self):
         self.subscribe(self.OnModelChanged, *[RideUserKeywordAdded,
                                               RideTestCaseAdded,
                                               RideOpenSuite,
@@ -142,11 +147,12 @@ class TestRunnerPlugin(Plugin):
         # this attempts to make sure the tree is always up-to-date
         self.subscribe(self.OnTabChanged, RideNotebookTabChanged)
 
-        port = self.settings["port"]
-        max_port = port+10
-        while not self._server and port <= max_port:
+    def _start_listener_server(self):
+        port = self.port
+        while not self._server and port <= port+10:
             try:
-                self._server = RideListenerServer(("",port), RideListenerHandler, self._post_result)
+                self._server = RideListenerServer(("",port), RideListenerHandler,
+                                                  self._post_result)
             except socket.error:
                 port += 1
                 continue
@@ -154,12 +160,16 @@ class TestRunnerPlugin(Plugin):
             self._server_thread.setDaemon(True)
             self._server_thread.start()
             self._port = port
+        if not self._server:
+            raise RuntimeError("Cannot start the RideListenerServer.")
+
+    def _load_tree_if_data_is_open(self):
         if self.model is not None and self.model.suite is not None:
             self.OnModelChanged()
-        
+
+    def _create_temporary_directory(self):
         self._tmpdir = tempfile.mkdtemp(".d", "RIDE")
         atexit.register(lambda: shutil.rmtree(self._tmpdir))
-
         # this plugin creates a temporary directory which _should_
         # get reaped at exit. Sometimes things happen which might
         # cause it to not get deleted. Maybe this would be a good 
@@ -167,14 +177,7 @@ class TestRunnerPlugin(Plugin):
         # signature and delete them if they are more than a few
         # days old...
 
-    def _read_run_profiles_from_config(self):
-        #Have to keep reference so that these classes are not garbage collected
-        self._profile_classes_from_config = [_RunProfile(name, run_prefix)
-                                             for name, run_prefix in self.settings["runprofiles"]]
-
     def disable(self):
-        '''Disable the plugin'''
-        self.active = False
         self._remove_from_notebook()
         self._remove_from_menubar()
         self._remove_from_toolbar()
@@ -207,37 +210,9 @@ class TestRunnerPlugin(Plugin):
         if self._process_timer:
             self._process_timer.Stop()
 
-    def OnSaveSettings(self, evt=None):
-        """Does the work of saving the current settings"""
-        self.settings.save()
-        self._save_timer = None
-        
-    def OnIncludeTagsChanged(self, evt):
-        '''Called when the user changes the data in the include tags text control'''
-        text = evt.GetString()
-        self.settings["include_tags"] = text
-        self.save_settings()
-
-    def OnExcludeTagsChanged(self, evt):
-        '''Called when the user changes the data in the exclude tags text control'''
-        text = evt.GetString()
-        self.settings["exclude_tags"] = text
-        self.save_settings()
-        
-    def OnExcludeCheckbox(self, evt):
-        '''Called when the user clicks on the "Exclude Tags" checkbox'''
-        self.settings["apply_exclude_tags"] = evt.IsChecked()
-        self.save_settings()
-
-    def OnIncludeCheckbox(self, evt):
-        '''Called when the user clicks on the "Include Tags" checkbox'''
-        self.settings["apply_include_tags"] = evt.IsChecked()
-        self.save_settings()
-
     def OnAutoSaveCheckbox(self, evt):
         '''Called when the user clicks on the "Auto Save" checkbox'''
-        self.settings["auto_save"] = evt.IsChecked()
-        self.save_settings()
+        self.save_setting("auto_save", evt.IsChecked())
 
     def OnStop(self, event):
         '''Called when the user clicks the "Stop" button
@@ -252,7 +227,7 @@ class TestRunnerPlugin(Plugin):
 
     def OnRun(self, event):
         '''Called when the user clicks the "Run" button'''
-        if self.settings["auto_save"]:
+        if self.auto_save:
             self._frame.OnSave(None)
 
         self._show_notebook_tab()
@@ -287,19 +262,16 @@ class TestRunnerPlugin(Plugin):
         if self._log_file:
             wx.LaunchDefaultBrowser("file:%s" % os.path.abspath(self._log_file))
 
-    def OnSplitterSashChanged(self,evt):
-        pos = self.splitter.GetSashPosition()
+    def OnSplitterSashChanged(self, evt):
+        sash_position = self.splitter.GetSashPosition()
         # this is a gross hack. For some reason yet to be determined, sometimes
         # a value of 7 is getting saved. Weird.
-        if pos > 10:
-            self.settings["sash"] = self.splitter.GetSashPosition()
-            self.save_settings()
+        if sash_position > 10:
+            self.save_setting("sash_position", sash_position)
 
     def OnProfileSelection(self, event):
-        profile = event.GetString()
-        self.SetProfile(profile)
-        self.settings["profile"] = profile
-        self.save_settings()
+        self.save_setting("profile", event.GetString())
+        self.SetProfile(self.profile)
 
     def OnDoubleClick(self, event):
         '''Handle double-click events on the tree
@@ -409,18 +381,6 @@ class TestRunnerPlugin(Plugin):
             self._output("\ntest finished %s" % now.strftime("%c"))
             self._set_state("stopped")
             self._process = None
-
-    def save_settings(self, delay=2000):
-        """Schedule the settings to be saved after a delay
-
-        The timer will be restarted if this function is called again
-        before the delay is up. This is to prevent saving the settings
-        multiple times (ie: on every keystroke)
-        """
-        if (not self._save_timer):
-            self._save_timer= wx.CallLater(delay, self.OnSaveSettings)
-        else:
-            self._save_timer.Restart(delay)
 
     def _clear(self):
         '''Clear the output window'''
@@ -648,7 +608,7 @@ class TestRunnerPlugin(Plugin):
 
         toolbar.EnableTool(ID_SHOW_LOG, False)
         toolbar.EnableTool(ID_SHOW_REPORT, False)
-        self.savecb.SetValue(bool(self.settings["auto_save"]))
+        self.savecb.SetValue(bool(self.auto_save))
 
         toolbar.Realize()
         toolbar.Bind(wx.EVT_TOOL, self.OnRun, id=ID_RUN)
@@ -759,7 +719,7 @@ class TestRunnerPlugin(Plugin):
         self.splitter.SplitVertically(self._left_panel, self._right_panel, 200)
         # I don't know why but the sash position is being ignored; setting
         # it via CallAfter seems to work
-        wx.CallAfter(self._set_splitter_size, self.settings["sash"])
+        wx.CallAfter(self._set_splitter_size, self.sash_position)
 
         self._process_timer = wx.Timer(self.panel)
 
