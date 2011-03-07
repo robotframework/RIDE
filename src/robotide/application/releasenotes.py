2a2d9e68b6f33031e4b0e59eb747a7e62b68a955
Mikko Korpela <mikko.korpela@gmail.com>
2011-03-07 16:50:37 +0200

    version 0.33 final

--- src/robotide/application/releasenotes.py
+++ src/robotide/application/releasenotes.py
@@ -84,4 +84,155 @@ WELCOME_TEXT = """
 # Release notes are updated automatically by package.py script whenever
 # a numbered distribution is created.
 RELEASE_NOTES = """
+<h2>Release notes for 0.33</h2>
+<table border="1">
+<tr>
+<td><b>ID</b></td>
+<td><b>Type</b></td>
+<td><b>Priority</b></td>
+<td><b>Summary</b></td>
+<td><b>AllLabels</b></td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=692">Issue 692</a></td>
+<td>Defect</td>
+<td>High</td>
+<td>Open Directory dialog losts focus on Windows</td>
+<td>Type-Defect, Priority-High, Target-0.33, OS-Windows, Usability</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=705">Issue 705</a></td>
+<td>Defect</td>
+<td>High</td>
+<td>test runner plugin fails on windows if cwd is root (C:\, D:\, etc)</td>
+<td>Type-Defect, Priority-High, OS-Windows, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=718">Issue 718</a></td>
+<td>Defect</td>
+<td>High</td>
+<td>Added keyword is not shown immediately</td>
+<td>Type-Defect, Priority-High, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=510">Issue 510</a></td>
+<td>Enhancement</td>
+<td>High</td>
+<td>Show all tags of a test case</td>
+<td>Type-Enhancement, Priority-High, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=669">Issue 669</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Ride cannot be used on small screen</td>
+<td>Type-Defect, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=690">Issue 690</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Tooltip shown above other applications</td>
+<td>Type-Defect, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=698">Issue 698</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Removing individually selected lines does not work correctly</td>
+<td>Type-Defect, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=704">Issue 704</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>A case where delete cells undo fails</td>
+<td>Type-Defect, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=710">Issue 710</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Duplicate modification in RIDE 0.32 if multiple keywords have same name</td>
+<td>Type-Defect, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=715">Issue 715</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Test Runner causes trace at startup</td>
+<td>Type-Defect, Priority-Medium, OS-OSX, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=722">Issue 722</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>User keyword and Test case names are not HTML escaped when serialized</td>
+<td>Type-Defect, Target-0.33, Priority-Medium</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=724">Issue 724</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Resource flickering during suite open</td>
+<td>Type-Defect, Target-0.33, Priority-Medium</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=726">Issue 726</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Importing library with second last argument other than string type fails</td>
+<td>Type-Defect, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=694">Issue 694</a></td>
+<td>Enhancement</td>
+<td>Medium</td>
+<td>Individual tag is hard to find from long list of tags</td>
+<td>Type-Enhancement, Priority-Medium, Target-0.33, Usability</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=695">Issue 695</a></td>
+<td>Enhancement</td>
+<td>Medium</td>
+<td>Editing tags is hard when there is lot of tags</td>
+<td>Type-Enhancement, Priority-Medium, Target-0.33, Usability</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=701">Issue 701</a></td>
+<td>Enhancement</td>
+<td>Medium</td>
+<td>Ability to print test suite at preview</td>
+<td>Type-Enhancement, Target-0.33, Priority-Medium</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=708">Issue 708</a></td>
+<td>Enhancement</td>
+<td>Medium</td>
+<td>make settings iterable</td>
+<td>Type-Enhancement, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=691">Issue 691</a></td>
+<td>Documentation</td>
+<td>Medium</td>
+<td>Embedded images are not loaded when too old wxpython version</td>
+<td>Type-Documentation, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=709">Issue 709</a></td>
+<td>Documentation</td>
+<td>Medium</td>
+<td>Document with wxPython 2.8.10.1 in Windows 7 Argument, Timeout and Return Value Dialog are opened twice</td>
+<td>Type-Documentation, Priority-Medium, Target-0.33</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=700">Issue 700</a></td>
+<td>Defect</td>
+<td>Low</td>
+<td>Delete shortcut is not working on Delete Rows</td>
+<td>Type-Defect, Priority-Low, Target-0.33</td>
+</tr>
+</table>
+<p>Altogether 20 issues.</p>
 """
