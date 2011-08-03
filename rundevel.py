b103fabf2c5680a20a83316d7e0797d2a9ffa265
Robot Framework Developers (robotframework@gmail.com) <Robot Framework Developers (robotframework@gmail.com)>
2011-08-03 17:52:35 +0300

    use the robotframework from bundled directory when running with rundevel.py

--- rundevel.py
+++ rundevel.py
@@ -3,8 +3,11 @@
 import os
 import sys
 
-src = os.path.join(os.path.dirname(__file__), 'src')
+ROOT = os.path.dirname(__file__)
+src = os.path.join(ROOT, 'src')
 sys.path.insert(0, src)
+bundled_robot = os.path.join(ROOT, 'bundled', 'robotframework', 'src')
+sys.path.insert(0, bundled_robot)
 
 from robotide import main
 
