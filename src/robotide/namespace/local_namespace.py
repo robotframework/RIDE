d4e5788cf1248e91bfa8e93480c61e92d26c51bd
Mikko Korpela <mikko.korpela@gmail.com>
2011-08-29 12:13:10 +0300

    cleanup

--- src/robotide/namespace/local_namespace.py
+++ src/robotide/namespace/local_namespace.py
@@ -41,16 +41,18 @@ class LocalRowNamespace(LocalMacroNamespace):
 
     def get_suggestions(self, start):
         suggestions = LocalMacroNamespace.get_suggestions(self, start)
-        if len(start) == 0 or start.startswith('$') or start.startswith('@'):
+        if self._could_be_variable(start):
             matching_assignments = set()
             for row, step in enumerate(self._controller.steps):
                 if self._row == row:
                     break
                 matching_assignments = matching_assignments.union(val.replace('=', '').strip() for val in step.assignments if val.startswith(start))
             if matching_assignments:
-                suggestions = suggestions + [LocalVariableInfo(name) for name in matching_assignments]
-                suggestions = sorted(suggestions)
+                suggestions = sorted(suggestions + [LocalVariableInfo(name) for name in matching_assignments])
         return suggestions
+
+    def could_be_variable(self, start):
+        return len(start) == 0 or start.startswith('$') or start.startswith('@')
 
     def has_name(self, value):
         if self._row:
