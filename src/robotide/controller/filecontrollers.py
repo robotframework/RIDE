a0bde27b3cfbbd773ebb55f267c815cf90a523ef
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-06-12 13:15:53 +0300

    Add directory resources only if data has a source

--- src/robotide/controller/filecontrollers.py
+++ src/robotide/controller/filecontrollers.py
@@ -425,15 +425,19 @@ class TestDataDirectoryController(_DataController, DirectoryController):
 
     def _children(self, data):
         children = [DataController(child, self._chief_controller, self) for child in data.children]
+        if data.source:
+            self._add_directory_resources(children, data)
+        return children
+
+    def _add_directory_resources(self, children, data):
         for filename in [os.path.join(data.source, f) for f in os.listdir(data.source)]:
-            if filename not in [c.source for c in children]+[self.data.initfile]:
+            if filename not in [c.source for c in children] + [self.data.initfile]:
                 resu = self._namespace.get_resource(filename, report_status=False)
                 if resu and (resu.setting_table or resu.variable_table or resu.keyword_table):
                     children.append(
                         self._resource_file_controller_factory.create(
                             resu,
                             chief_controller=self._chief_controller))
-        return children
 
     def add_child(self, controller):
         self.children.append(controller)
