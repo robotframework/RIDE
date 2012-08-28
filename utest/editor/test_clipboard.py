46c9be14713270f2da590a9a7e74b9621b7377f1
Mika Hänninen <mika.hanninen@iki.fi>
2012-08-28 15:12:09 +0300

    Clipboard related tests that work inconsistently on the Windows are now skipped.

--- utest/editor/test_clipboard.py
+++ utest/editor/test_clipboard.py
@@ -18,27 +18,28 @@ import unittest
 from robot.utils.asserts import assert_equals
 
 from resources import PYAPP_REFERENCE as _ #Needed to be able to create wx components
+from robotide.context.platform import IS_WINDOWS
 from robotide.editor.clipboard import _GridClipboard
 
+if not IS_WINDOWS:
+    class TestGridClipBoard(unittest.TestCase):
 
-class TestGridClipBoard(unittest.TestCase):
+        def test_with_string_content(self):
+            self._test_clipboard('Hello, world!', 'Hello, world!')
 
-    def test_with_string_content(self):
-        self._test_clipboard('Hello, world!', 'Hello, world!')
+        def test_with_list_content(self):
+            self._test_clipboard([['Hello', 'world!']], 'Hello\tworld!')
 
-    def test_with_list_content(self):
-        self._test_clipboard([['Hello', 'world!']], 'Hello\tworld!')
+        def test_with_multiple_rows(self):
+            self._test_clipboard([['Hello', 'world!'], ['Another', 'row']],
+                                 'Hello\tworld!\nAnother\trow')
 
-    def test_with_multiple_rows(self):
-        self._test_clipboard([['Hello', 'world!'], ['Another', 'row']],
-                             'Hello\tworld!\nAnother\trow')
-
-    def _test_clipboard(self, content, expected=''):
-        clipb = _GridClipboard()
-        clipb.set_contents(content)
-        assert_equals(clipb._get_contents(),
-                      expected.replace('\n', os.linesep))
+        def _test_clipboard(self, content, expected=''):
+            clipb = _GridClipboard()
+            clipb.set_contents(content)
+            assert_equals(clipb._get_contents(),
+                          expected.replace('\n', os.linesep))
 
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
