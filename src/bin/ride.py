a9aee05c956e3078a4756dd7f00afa6b8de10bb8
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-09-19 09:56:43 +0300

    ride.py: fix multiprocessing in windows

--- src/bin/ride.py
+++ src/bin/ride.py
@@ -19,4 +19,9 @@ import sys
 
 from robotide import main
 
-main(sys.argv[1:])
+# Must be protected against reimporting
+# As multiprocessing has an odd requirement
+# and we use multiprocessing
+# http://docs.python.org/library/multiprocessing.html#windows
+if __name__ == '__main__':
+    main(sys.argv[1:])
