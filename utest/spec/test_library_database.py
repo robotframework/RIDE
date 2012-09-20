e6a63c60d30a052c3f08a13d531b777348fea78c
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-09-20 10:31:33 +0300

    more db tests

--- utest/spec/test_library_database.py
+++ utest/spec/test_library_database.py
@@ -8,30 +8,70 @@ from robotide.spec.libraryfetcher import _get_keywords
 
 class TestLibraryDatabase(unittest.TestCase):
 
-    def test_testing(self):
-        conn = sqlite3.connect(':memory:')
+    def setUp(self):
+        self._connection = sqlite3.connect(':memory:')
         with open(os.path.join(os.path.split(spec.__file__)[0], 'create_database.sql')) as script:
-            conn.executescript(script.read())
-        collections_kws = self._get_and_insert_keywords(conn, 'Collections', '')
-        string_kws = self._get_and_insert_keywords(conn, 'String', '')
-        collections_kws_from_db = self._fetch_keywords_from_db(conn, 'Collections', '')
-        string_kws_from_db = self._fetch_keywords_from_db(conn, 'String', '')
+            self._connection.executescript(script.read())
+
+    def tearDown(self):
+        self._connection.close()
+
+    def test_inserting_and_fetching(self):
+        collections_kws = self._get_and_insert_keywords('Collections', '')
+        string_kws = self._get_and_insert_keywords('String', '')
+        collections_kws_from_db = self._fetch_keywords_from_db('Collections', '')
+        string_kws_from_db = self._fetch_keywords_from_db('String', '')
         self._check_keywords(collections_kws, collections_kws_from_db)
         self._check_keywords(string_kws, string_kws_from_db)
 
-    def _get_and_insert_keywords(self, conn, library_name, library_arguments):
+    def test_finds_newest_version(self):
+        lib = self._insert_library('lib.py', 'foo')
+        self._insert_library_keywords([['this is old', 'doc', '', lib[0]]])
+        lib = self._insert_library('lib.py', 'foo')
+        self._insert_library_keywords([['this is new', 'doc', '', lib[0]]])
+        kws = self._fetch_keywords_from_db('lib.py', 'foo')
+        self.assertEqual(len(kws), 1)
+        self.assertEqual(kws[0].name, 'this is new')
+
+    def test_removing_old_data(self):
+        self._get_and_insert_keywords('String', '')
+        self._get_and_insert_keywords('String', '')
+        newest = self._get_and_insert_keywords('String', '')
+        self._remove_old_library_data('String', '')
+        newest_again = self._fetch_keywords_from_db('String', '')
+        self._check_keywords(newest, newest_again)
+        count = self._connection.execute('select count(*) from libraries').fetchone()[0]
+        self.assertEqual(count, 1)
+
+    def _remove_old_library_data(self, name, arguments):
+        new = self._fetch_lib(name, arguments)
+        old_versions = self._connection.execute('select id from libraries where name = ? and arguments = ? and id != ?', (name, arguments, new[0])).fetchall()
+        self._connection.executemany('delete from keywords where library = ?', old_versions)
+        self._connection.execute('delete from libraries where name = ? and arguments = ? and id != ?', (name, arguments, new[0]))
+
+    def _get_and_insert_keywords(self, library_name, library_arguments):
         kws = _get_keywords(library_name, library_arguments)
-        conn.execute('insert into libraries values (null, ?, ?, ?)', (library_name, library_arguments, time.time()))
-        lib = [l for l in conn.execute('select * from libraries where name = ? and arguments = ?', (library_name, library_arguments))][0]
+        lib = self._insert_library(library_name, library_arguments)
         keyword_values = [[kw.name, kw.doc, u' | '.join(kw.arguments), lib[0]] for kw in kws]
-        conn.executemany('insert into keywords values (?, ?, ?, ?)', keyword_values)
+        self._insert_library_keywords(keyword_values)
         return kws
 
-    def _fetch_keywords_from_db(self, conn, library_name, library_arguments):
-        lib = [l for l in conn.execute('select * from libraries where name = ? and arguments = ?', (library_name, library_arguments))][0]
+    def _insert_library(self, name, arguments):
+        self._connection.execute('insert into libraries values (null, ?, ?, ?)', (name, arguments, time.time()))
+        return self._fetch_lib(name, arguments)
+
+    def _fetch_lib(self, name, arguments):
+        t = self._connection.execute('select max(last_updated) from libraries where name = ? and arguments = ?', (name, arguments)).fetchone()[0]
+        return self._connection.execute('select * from libraries where name = ? and arguments = ? and last_updated = ?', (name, arguments, t)).fetchone()
+
+    def _insert_library_keywords(self, data):
+        self._connection.executemany('insert into keywords values (?, ?, ?, ?)', data)
+
+    def _fetch_keywords_from_db(self, library_name, library_arguments):
+        lib = self._fetch_lib(library_name, library_arguments)
         return [LibraryKeywordInfo(name, doc, lib[1], arguments.split(u' | '))
                 for name, doc, arguments in
-                conn.execute('select name, doc, arguments from keywords where library = ?', [lib[0]])]
+                self._connection.execute('select name, doc, arguments from keywords where library = ?', [lib[0]])]
 
     def _check_keywords(self, originals, from_database):
         for k1, k2 in zip(originals, from_database):
