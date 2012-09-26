2d37b15d3f620968002acb30d4b4a665f722b9be
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-09-26 14:31:58 +0300

    test_plugin_api: add LibraryManager

--- utest/plugin/test_plugin_api.py
+++ utest/plugin/test_plugin_api.py
@@ -8,6 +8,7 @@ from robotide.robotapi import TestCaseFile
 from robotide.controller.filecontrollers import DataController
 
 from resources import FakeApplication
+from robotide.spec.librarymanager import LibraryManager
 
 
 class ContentAssistPlugin(Plugin):
@@ -23,9 +24,14 @@ class TestContentAssistHook(unittest.TestCase):
     def test_hook_suggestions_are_included(self):
         self.app = FakeApplication()
         self.app.namespace = Namespace(self.app.settings)
+        library_manager = LibraryManager(':memory:')
+        library_manager.create_database()
+        library_manager.start()
+        self.app.namespace.set_library_manager(library_manager)
         pl = ContentAssistPlugin(self.app, name='test')
         pl.register_content_assist_hook(pl._get_content_assist_values)
         self._assert_contains('foo')
+        library_manager.stop()
 
     def _assert_contains(self, name):
         controller = DataController(TestCaseFile(), None)
