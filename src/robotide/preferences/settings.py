21bd13689b899a1335ef4dd765fce5c277876ed9
Tatu Kairi <tatu.kairi@eficode.com>
2012-09-27 14:42:35 +0300

    Settings: read project's excludes from exclude file

--- src/robotide/preferences/settings.py
+++ src/robotide/preferences/settings.py
@@ -230,7 +230,6 @@ class Settings(_Section):
         for l in self._listeners:
             l.setting_changed(name, old_value, new_value)
 
-
 class RideSettings(Settings):
 
     def __init__(self):
@@ -239,7 +238,25 @@ class RideSettings(Settings):
         Settings.__init__(self, user_path)
         self._settings_dir = os.path.dirname(user_path)
         self.set('install root', os.path.dirname(os.path.dirname(__file__)))
+        self._excludes = _get_excludes(SETTINGS_DIRECTORY, self.get('default directory', None))
 
     def get_path(self, *parts):
         """Returns path which combines settings directory and given parts."""
         return os.path.join(self._settings_dir, *parts)
+
+def _get_project_name(project_dir):
+    if not project_dir:
+        raise Exception('lol') #TODO FIXME
+    project_dir = project_dir[:-1] if project_dir.endswith('/') else project_dir # strip trailing slash
+    project_name = os.path.split(project_dir)[-1]
+    return project_name
+
+def _get_excludes(settings_dir, project_dir):
+    project_name = _get_project_name(project_dir)
+    exclude_file_path = os.path.join(settings_dir, project_name)
+    excludes = None
+    try:
+        excludes = open(exclude_file_path, 'w').read().split()
+    except IOError:
+        raise Exception('lol') #TODO FIXME
+    return excludes
