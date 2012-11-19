38102775f5ddb4579b3e4007afced5878b496162
Tatu Kairi <tatu.kairi@eficode.com>
2012-11-19 13:54:46 +0200

    Version 0.53

--- src/robotide/application/releasenotes.py
+++ src/robotide/application/releasenotes.py
@@ -86,7 +86,7 @@ WELCOME_TEXT = """
 # Release notes are updated automatically by package.py script whenever
 # a numbered distribution is created.
 RELEASE_NOTES = """
-<h2>Release notes for 0.52.1</h2>
+<h2>Release notes for 0.53</h2>
 <table border="1">
 <tr>
 <td><p><b>ID</b></p></td>
@@ -95,17 +95,89 @@ RELEASE_NOTES = """
 <td><p><b>Summary</b></p></td>
 </tr>
 <tr>
-<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1100">Issue 1100</a></td>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1102">Issue 1102</a></td>
 <td>Defect</td>
 <td>High</td>
-<td>Exclude will show debug prints in console</td>
+<td>RIDE doesn't prompt to save changes when switching between projects</td>
 </tr>
 <tr>
-<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=580">Issue 580</a></td>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=875">Issue 875</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>test runner listener interface doesn't work on windows with iron python</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=983">Issue 983</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Unable to copy/paste from View Log screen</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=998">Issue 998</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Rename error when renaming KW</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1090">Issue 1090</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>RIDE hangs up while test execution</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1099">Issue 1099</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>New lines in user keyword name or variable name breaks the Tree view</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1107">Issue 1107</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Exclude does not work if the directory has a __init__ file</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1108">Issue 1108</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Editing a cell exits after a very short amount of time</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1109">Issue 1109</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>file 'dirty' status behaves badly with exclude</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1111">Issue 1111</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>RIDE crashes if a suite file name contains newlines</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1113">Issue 1113</a></td>
+<td>Defect</td>
+<td>Medium</td>
+<td>Renaming suite should update tests selected for running</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=816">Issue 816</a></td>
 <td>Enhancement</td>
 <td>Medium</td>
-<td>User keyword Arguments should be presented in a more userfriendly manner</td>
+<td>Expand All/Collapse All actions to tree nodes</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1071">Issue 1071</a></td>
+<td>Enhancement</td>
+<td>Medium</td>
+<td>support change font</td>
+</tr>
+<tr>
+<td><a href="http://code.google.com/p/robotframework-ride/issues/detail?id=1078">Issue 1078</a></td>
+<td>Enhancement</td>
+<td>Medium</td>
+<td>Caret moves to the top of text edit when change between edit- and text edit-tabs</td>
 </tr>
 </table>
-<p>Altogether 2 issues.</p>
+<p>Altogether 14 issues.</p>
 """
