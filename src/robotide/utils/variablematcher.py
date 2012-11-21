237d85c484cba1ddec646ea1b1d17c3b3b083146
Tatu Kairi <tatu.kairi@eficode.com>
2012-11-21 16:54:30 +0200

    Fixes #1080: 'Find Where Used' in Grid now recognizes dashes in variable name

--- src/robotide/utils/variablematcher.py
+++ src/robotide/utils/variablematcher.py
@@ -48,7 +48,7 @@ def get_variable_basename(value):
     "Return variable without extended variable syntax part"
     if is_list_variable(value):
         return get_variable(value)
-    match = re.match('\${(.+?)[^\s\w]+.*?}?', value)
+    match = re.match('\${(.+?)[^\s\w-]+.*?}?', value)
     if not match:
         return None
     return '${%s}' % (match.groups()[0].strip())
