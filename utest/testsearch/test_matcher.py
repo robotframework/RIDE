c106738f32ed94be497399a15541b48edbd354be
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-11-23 15:42:02 +0200

    tests for matching tags and doc

--- utest/testsearch/test_matcher.py
+++ utest/testsearch/test_matcher.py
@@ -25,18 +25,33 @@ class TestTestSearchMatcher(unittest.TestCase):
     def test_matching_name(self):
         self.assertTrue(self._matcher.matches(self._test('name')))
 
-    def test_not_matching_name(self):
-        self.assertFalse(self._matcher.matches(self._test('unknown')))
+    def test_not_matching(self):
+        self.assertFalse(self._matcher.matches(self._test('unknown', tags=['no match'], doc='no match')))
 
-    def _test(self, name, tags=None, doc=''):
+    def test_matching_name_partially(self):
+        self.assertTrue(self._matcher.matches(self._test('prefix[name]postfix')))
+
+    def test_matching_name_is_case_insensitive(self):
+        self.assertTrue(self._matcher.matches(self._test('NamE')))
+
+    def test_matching_to_documentation(self):
+        self.assertTrue(self._matcher.matches(self._test('SOME', doc='There is some name')))
+
+    def test_matching_to_tag(self):
+        self.assertTrue(self._matcher.matches(self._test('SOME', tags=['name'])))
+
+    def _test(self, name, tags=None, doc='documentation'):
         parent = lambda:0
         parent.datafile_controller = parent
         parent.register_for_namespace_updates = lambda *_:0
         parent.force_tags = []
         parent.default_tags = []
         robot_test = TestCase(parent=parent, name=name)
+        robot_test.get_setter('documentation')(doc)
+        robot_test.get_setter('tags')(tags or [])
         test = TestCaseController(parent, robot_test)
         return test
+
 
 if __name__ == '__main__':
     unittest.main()
