5ad4c3968cd1ee998750110a6b92c7943c858f60
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-11-23 09:24:49 +0200

    working draft of test search

---
+++ src/robotide/testsearch/testsdialog.py
@@ -0,0 +1,35 @@
+#  Copyright 2008-2012 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from robotide.widgets import Dialog, VerticalSizer, VirtualList
+
+
+class TestsDialog(Dialog):
+
+    def __init__(self, name, tests):
+        self._name = name
+        self._selection_listeners = []
+        title = "Tests matching '%s'" % (name)
+        Dialog.__init__(self, title=title, size=(650, 400))
+        self.SetSizer(VerticalSizer())
+        self.tests = tests
+        self.tests_list = VirtualList(self, ['Tests'], self.tests)
+        self.tests_list.add_selection_listener(self._usage_selected)
+        self.Sizer.add_expanding(self.tests_list)
+
+    def _usage_selected(self, idx):
+        for listener in self._selection_listeners:
+            listener(self.tests[idx])
+
+    def add_selection_listener(self, listener):
+        self._selection_listeners.append(listener)
--- src/robotide/testsearch/testsearch.py
+++ src/robotide/testsearch/testsearch.py
@@ -12,7 +12,10 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 from robotide.pluginapi import Plugin
+from robotide.testsearch.testsdialog import TestsDialog
+from robotide.utils import overrides
 from robotide.widgets import ImageProvider
+from robotide.widgets.list import ListModel
 
 
 class TestSearchPlugin(Plugin):
@@ -21,11 +24,34 @@ class TestSearchPlugin(Plugin):
         self.register_search_action('Search Tests', self.show_search_for, ImageProvider().TEST_SEARCH_ICON)
 
     def show_search_for(self, text):
-        def etsii(data):
-            for t in data.tests:
-                if text in t.name or text in [str(tag) for tag in t.tags]:
-                    print t.longname
-            for s in data.suites:
-                etsii(s)
-        etsii(self.frame._controller.data)
+        d = TestsDialog(text, _TestSearchListModel(self._search(text, self.frame._controller.data)))
+        d.add_selection_listener(self._foo)
+        d.Show()
+
+    def _foo(self, test):
+        self.tree.select_node_by_data(test)
+
+    def _search(self, text, data):
+        for test in data.tests:
+            if text in test.name or text in [str(tag) for tag in test.tags]:
+                yield test
+        for s in data.suites:
+            for test in self._search(text, s):
+                yield test
+
+class _TestSearchListModel(ListModel):
+
+    def __init__(self, tests):
+        self._tests = list(tests)
+
+    @property
+    @overrides(ListModel)
+    def count(self):
+        return len(self._tests)
+
+    def __getitem__(self, item):
+        return self._tests[item]
+
+    def item_text(self, row, col):
+        return self._tests[row].longname
 
