4cbe65bdda254de30f116426888c2845b65bd9f3
Ilmari Kontulainen <kontulai@gmail.com>
2012-11-27 08:42:02 -0600

    add desktop icon in windows installer

--- post_install.py
+++ post_install.py
@@ -1,5 +1,8 @@
 import sys
-if sys.argv[1] == '-install':
+from os.path import join
+
+
+def verify_install():
     try:
         import wxversion
     except ImportError:
@@ -7,3 +10,17 @@ if sys.argv[1] == '-install':
         print ""
         print "Please ensure that you have wxPython installed before running RIDE."
         print "You can obtain wxPython from http://wxpython.org/"
+    else:
+        print "Installation successful."
+
+
+def create_desktop_shortcut():
+    link = join(get_special_folder_path("CSIDL_DESKTOPDIRECTORY"), 'RIDE.lnk')
+    icon = join(sys.prefix, 'Lib', 'site-packages', 'robotide', 'widgets', 'robot.ico')
+    create_shortcut('pythonw', "Robot Framework testdata editor", link,
+                    '-c "from robotide import main; main()"', '', icon)
+
+
+if sys.argv[1] == '-install':
+    verify_install()
+    create_desktop_shortcut()
