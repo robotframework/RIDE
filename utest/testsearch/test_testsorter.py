82db877b572a3472c1f0a45f647db93906680974
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-12-03 13:24:42 +0200

    TestSearch: Using multimatcher

--- utest/testsearch/test_testsorter.py
+++ utest/testsearch/test_testsorter.py
@@ -18,32 +18,37 @@ from testsearch.test_matcher import _TestSearchTest
 class TestTestSorter(_TestSearchTest, unittest.TestCase):
 
     def test_exact_match_is_better_than_partial(self):
-        self._matches_in_order('exact', ['exact', 'exact jotain', 'jotain exact', 'exact_foo', 'aexact', 'zexact'])
+        self._matches_in_order('*exact*', ['aexact', 'exact', 'exact jotain', 'exact_foo', 'jotain exact', 'zexact'])
 
     def _matches_in_order(self, match_text, matches):
         match_objects = [self._match(match_text, name=name) for name in matches]
         for i in range(1, len(match_objects)):
-            self.assertTrue(match_objects[i] > match_objects[i-1])
+            self._assert_is_greater(match_objects[i], match_objects[i-1])
 
     def test_name_is_better_than_doc(self):
         name_match = self._match('name', name='name')
         doc_match = self._match('doc', doc='doc')
-        self.assertTrue(doc_match > name_match)
+        self._assert_is_greater(doc_match, name_match)
 
     def test_name_is_better_than_tag(self):
         name_match = self._match('name', name='name')
         tag_match = self._match('tag', tags=['tag'])
-        self.assertTrue(tag_match > name_match)
+        self._assert_is_greater(tag_match, name_match)
 
     def test_tag_is_better_than_doc(self):
         tag_match = self._match('tag', tags=['tag'])
         doc_match = self._match('doc', doc='doc')
-        self.assertTrue(doc_match > tag_match)
+        self._assert_is_greater(doc_match, tag_match)
 
     def test_tags_order(self):
-        tag1_match = self._match('tag', tags=['atag'])
-        tag2_match = self._match('tag', tags=['btag'])
-        self.assertTrue(tag2_match > tag1_match)
+        tag1_match = self._match('*tag', tags=['atag'])
+        tag2_match = self._match('*tag', tags=['btag'])
+        self._assert_is_greater(tag2_match, tag1_match)
+
+    def _assert_is_greater(self, greater, smaller):
+        self.assertTrue(greater > smaller, msg='%r !>! %r' % (greater, smaller))
+        self.assertFalse(smaller > greater)
+        self.assertFalse(greater == smaller)
 
 
 if __name__ == '__main__':
