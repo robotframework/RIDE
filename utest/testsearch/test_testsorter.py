c0bb14e1c362d44fd0cde3759b77e15e40a56a47
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-12-05 12:47:47 +0200

    testsearch: fix tests

--- utest/testsearch/test_testsorter.py
+++ utest/testsearch/test_testsorter.py
@@ -18,7 +18,7 @@ from testsearch.test_matcher import _TestSearchTest
 class TestTestSorter(_TestSearchTest, unittest.TestCase):
 
     def test_exact_match_is_better_than_partial(self):
-        self._matches_in_order('*exact*', ['aexact', 'exact', 'exact jotain', 'exact_foo', 'jotain exact', 'zexact'])
+        self._matches_in_order('exact', ['aexact', 'exact', 'exact jotain', 'exact_foo', 'jotain exact', 'zexact'])
 
     def _matches_in_order(self, match_text, matches):
         match_objects = [self._match(match_text, name=name) for name in matches]
@@ -26,21 +26,21 @@ class TestTestSorter(_TestSearchTest, unittest.TestCase):
             self._assert_is_greater(match_objects[i], match_objects[i-1])
 
     def test_all_matches_is_better_than_some(self):
-        self._matches_in_order('zoo* *foo* *bar', ['zoo foo bar', 'zoo foo', 'bar'])
+        self._matches_in_order('zoo foo bar', ['zoo foo bar', 'zoo foo', 'bar'])
 
     def test_more_matches_is_better_than_some_in_name(self):
-        all_matches_in_docs = self._match('*foo* *bar*', doc='foo bar')
-        some_match_in_name = self._match('*foo* *bar*', name='foo')
+        all_matches_in_docs = self._match('foo bar', doc='foo bar')
+        some_match_in_name = self._match('foo bar', name='foo')
         self._assert_is_greater(some_match_in_name, all_matches_in_docs)
 
     def test_more_matches_is_better_than_some_in_tags(self):
-        some_match_in_tag = self._match('*foo* *bar*', tags=['bar'])
-        all_matches_in_tags = self._match('*foo* *bar*', tags=['foo', 'bar'])
+        some_match_in_tag = self._match('foo bar', tags=['bar'])
+        all_matches_in_tags = self._match('foo bar', tags=['foo', 'bar'])
         self._assert_is_greater(some_match_in_tag, all_matches_in_tags)
 
     def test_same_pattern_matches_do_not_raise_priority(self):
-        all_matches_in_name = self._match('*foo* *bar*', name='foo bar')
-        some_match_in_doc = self._match('*foo* *bar*', doc='bar bar bar bar bar bar')
+        all_matches_in_name = self._match('foo bar', name='foo bar')
+        some_match_in_doc = self._match('foo bar', doc='bar bar bar bar bar bar')
         self._assert_is_greater(some_match_in_doc, all_matches_in_name)
 
     def test_name_is_better_than_doc(self):
@@ -59,8 +59,8 @@ class TestTestSorter(_TestSearchTest, unittest.TestCase):
         self._assert_is_greater(doc_match, tag_match)
 
     def test_tags_order(self):
-        tag1_match = self._match('*tag', tags=['atag'])
-        tag2_match = self._match('*tag', tags=['btag'])
+        tag1_match = self._match('tag', tags=['atag'])
+        tag2_match = self._match('tag', tags=['btag'])
         self._assert_is_greater(tag2_match, tag1_match)
 
     def _assert_is_greater(self, greater, smaller):
