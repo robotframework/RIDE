d375542b419458423656a52b57c506c41791dbc8
Pekka Klärck <peke@iki.fi>
2012-12-07 15:18:49 +0200

    Updated app icon pack to contain also 64x64 version of the icon. Need to still test how it looks in different environments. Also added accidentally deleted create_icon.sh back.

---
+++ tools/create_icon.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Download base image if needed
+if [ ! -e robot-trans.png ]; then
+    wget http://robotframework.googlecode.com/hg/doc/images/robot-trans.png
+fi
+
+# Cut base image and save it as PNM and as alpha channel
+cut="28 57 180 180"
+pngtopnm -mix robot-trans.png | pamcut $cut > robot.pnm
+pngtopnm -alpha robot-trans.png | pamcut $cut | ppmtopgm > alpha.pgm
+
+# Create input images (and alpha channels) for icon in different sizes
+for size in 16 32 48 64; do
+    pnmscale -xysize $size $size robot.pnm > tmp.pnm
+    pnmquant 16 tmp.pnm > $size.pnm  # pnmquant fails to work with stdin
+    pnmscale -xysize $size $size alpha.pgm > $size.pgm
+done
+
+# Create the icon
+ppmtowinicon -andpgms \
+    16.pnm 16.pgm \
+    32.pnm 32.pgm \
+    48.pnm 48.pgm \
+    64.pnm 64.pgm \
+    > robot.ico
+echo "Created robot.ico"
+
+# Cleanup
+rm *.pnm *.pgm
