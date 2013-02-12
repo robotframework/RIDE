e684ada4f4f11e625ee981ae33f6ad48d719fd9b
Mikko Korpela <mikko.korpela@reaktor.fi>
2013-02-12 14:03:16 +0200

    test_debugger: Use context manager to better show keyword structure

--- utest/contrib/testrunner/test_debugger.py
+++ utest/contrib/testrunner/test_debugger.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import threading
 import unittest
 import time
@@ -5,17 +6,18 @@ from robotide.contrib.testrunner.TestRunnerAgent import RobotDebugger
 
 class TestDebugger(unittest.TestCase):
 
+    def setUp(self):
+        self._debugger = RobotDebugger()
+
     def test_pausing_and_resuming(self):
-        debugger = RobotDebugger()
-        self.assertFalse(debugger.is_paused())
-        debugger.pause()
-        self.assertTrue(debugger.is_paused())
-        debugger.resume()
-        self.assertFalse(debugger.is_paused())
+        self.assertFalse(self._debugger.is_paused())
+        self._debugger.pause()
+        self.assertTrue(self._debugger.is_paused())
+        self._debugger.resume()
+        self.assertFalse(self._debugger.is_paused())
 
     def test_step_next(self):
-        debugger = RobotDebugger()
-        debugger.pause()
+        self._debugger.pause()
         started = threading.Event()
         first_keyword_done = threading.Event()
         second_keyword_done = threading.Event()
@@ -24,28 +26,25 @@ class TestDebugger(unittest.TestCase):
 
         def test_execution():
             started.set()
-            debugger.start_keyword()
-            first_keyword_done.set()
-            wait_for_step_next_before_entering_debugger.wait()
-            debugger.start_keyword()
-            second_keyword_done.set()
-            debugger.end_keyword()
-            debugger.end_keyword()
-            debugger.start_keyword()
-            third_keyword_done.set()
-            debugger.end_keyword()
+            with self.kw():
+                first_keyword_done.set()
+                wait_for_step_next_before_entering_debugger.wait()
+                with self.kw():
+                    second_keyword_done.set()
+            with self.kw():
+                third_keyword_done.set()
 
         def debugger_signals():
             self._verify_done(started)
             self.assertFalse(first_keyword_done.isSet())
-            debugger.step_next()
+            self._debugger.step_next()
             self._verify_done(first_keyword_done)
             self.assertFalse(second_keyword_done.isSet())
-            debugger.step_next()
+            self._debugger.step_next()
             wait_for_step_next_before_entering_debugger.set()
             self._verify_done(second_keyword_done)
             self.assertFalse(third_keyword_done.isSet())
-            debugger.step_next()
+            self._debugger.step_next()
             self._verify_done(third_keyword_done)
 
         t = threading.Thread(target=test_execution)
@@ -57,9 +56,14 @@ class TestDebugger(unittest.TestCase):
     def _verify_done(self, event):
         self.assertTrue(event.wait(timeout=1.0) or event.isSet())
 
+    @contextmanager
+    def kw(self):
+        self._debugger.start_keyword()
+        yield
+        self._debugger.end_keyword()
+
     def test_step_over(self):
-        debugger = RobotDebugger()
-        debugger.pause()
+        self._debugger.pause()
         started = threading.Event()
         first_keyword_done = threading.Event()
         second_keyword_done = threading.Event()
@@ -67,30 +71,25 @@ class TestDebugger(unittest.TestCase):
 
         def test_execution():
             started.set()
-            debugger.start_keyword()
-            first_keyword_done.set()
-            debugger.start_keyword()
-            second_keyword_done.set()
-            debugger.start_keyword()
-            debugger.end_keyword()
-            debugger.start_keyword()
-            debugger.end_keyword()
-            debugger.end_keyword()
-            debugger.start_keyword()
-            debugger.end_keyword()
-            debugger.end_keyword()
-            debugger.start_keyword()
-            last_keyword_done.set()
-            debugger.end_keyword()
+            with self.kw():
+                first_keyword_done.set()
+                with self.kw():
+                    second_keyword_done.set()
+                with self.kw():
+                    pass
+                with self.kw():
+                    pass
+            with self.kw():
+                last_keyword_done.set()
 
         def debugger_signals():
             self._verify_done(started)
             self.assertFalse(first_keyword_done.isSet())
-            debugger.step_over()
+            self._debugger.step_over()
             self._verify_done(first_keyword_done)
             self._verify_done(second_keyword_done)
             self.assertFalse(last_keyword_done.isSet())
-            debugger.step_over()
+            self._debugger.step_over()
             self._verify_done(last_keyword_done)
 
         t = threading.Thread(target=test_execution)
