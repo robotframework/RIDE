e5ab40cf8d70c73a4cd4e616c5fdbf601a05dc2c
Mikko Korpela <mikko.korpela@gmail.com>
2013-11-29 08:11:12 +0100

    simplifier: improve

--- rtest/simplifier.py
+++ rtest/simplifier.py
@@ -1,5 +1,11 @@
 
-def simplify(min_i, trace, runner):
+def simplify(trace, runner):
+    try:
+        return _simplify(1, trace, runner)
+    except ResetSimplify, reset:
+        return simplify(reset.trace, runner)
+
+def _simplify(min_i, trace, runner):
     max_i = len(trace)
     if max_i == min_i:
         return trace
@@ -9,9 +15,16 @@ def simplify(min_i, trace, runner):
     while start < len(un_trace):
         new_trace = un_trace[:start]+un_trace[start+step:]+[trace[-1]]
         if test_trace(new_trace, runner):
-            return simplify(min_i, new_trace, runner)
+            return _simplify(min_i, new_trace, runner)
         start += step
-    return simplify(min_i+1, trace, runner)
+    return _simplify(min_i+1, trace, runner)
+
+class ResetSimplify(Exception):
+
+    def __init__(self, trace):
+        Exception.__init__(self)
+        self.trace = trace
+
 
 def test_trace(trace, runner):
     runner.initialize()
@@ -19,6 +32,8 @@ def test_trace(trace, runner):
         run_trace(runner, trace)
         return False
     except Exception:
+        if runner.count <= trace[-1]:
+            raise ResetSimplify([i for i in trace if i < runner.count])
         return True
 
 def run_trace(runner, trace):
@@ -40,16 +55,19 @@ if __name__ == '__main__':
             self._original_data = data
             self._fails = data[-1]
             self._data = data[:-1]
+            self.count = 0
 
         def initialize(self):
             self.__init__(self._original_data)
 
         def step(self):
+            self.count += 1
             if (not self._data) and (not self._fails):
                 return
             self._data.pop(0)
 
         def skip_step(self):
+            self.count += 1
             d = self._data.pop(0)
             self._fails &= (not d)
 
@@ -62,7 +80,7 @@ if __name__ == '__main__':
         runner = Runner(test_data)
         trace = range(10000)
         print '!!'
-        optimal_trace = simplify(1, trace, runner)
+        optimal_trace = simplify(trace, runner)
         print optimal_trace
         print '--'
         for n in optimal_trace:
