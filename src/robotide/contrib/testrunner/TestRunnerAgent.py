04d4fbfb6dd64790ed37986e74aedf2b3145cb55
Janne Härkönen <jth@reaktor.fi>
2015-05-22 11:51:19 +0300

    Update Copyright year

--- src/robotide/contrib/testrunner/TestRunnerAgent.py
+++ src/robotide/contrib/testrunner/TestRunnerAgent.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Modified by Mikko Korpela under NSN copyrights
-#  Copyright 2008-2012 Nokia Solutions and Networks
+#  Copyright 2008-2015 Nokia Solutions and Networks
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
