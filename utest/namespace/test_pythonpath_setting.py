702bb0373aecf9d4b77b43bc12f5437995d42347
Janne Härkönen <janne.harkonen@iki.fi>
2015-07-24 13:10:28 +0300

    Fix unit tests

--- utest/namespace/test_pythonpath_setting.py
+++ utest/namespace/test_pythonpath_setting.py
@@ -14,6 +14,6 @@ def test_bundled_libraries_path_is_in_sys_path_by_default():
 @patch.object(robotlibraryloader, 'find_installed_robot_libraries')
 def test_bundle_path_is_removed_when_using_installed_rf_libraries(mock_func):
     namespace.Namespace(FakeSettings({'use installed robot libraries': True}))
-    mock_func.assert_called()
+    mock_func.assert_called_once_with(None)
     assert_true(namespace.REMOTE_LIB_PATH in sys.path)
     assert_false(namespace.BUNDLED_LIBRARIES_PATH in sys.path)
