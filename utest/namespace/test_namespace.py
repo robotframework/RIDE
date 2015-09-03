015d3ffe7fa1a633f1e63249e0201a249b45b852
Janne Härkönen <janne.harkonen@iki.fi>
2015-09-03 10:15:46 +0300

    pep8

--- utest/namespace/test_namespace.py
+++ utest/namespace/test_namespace.py
@@ -38,6 +38,7 @@ EXISTING_USER_KEYWORD = 'Should be in keywords Uk'
 COLLIDING_ARGUMENT = '${colliding argument}'
 COLLIDING_CONSTANT = COLLIDING_ARGUMENT.upper()
 
+
 def _build_test_case_file():
     tcf = TestCaseFile()
     tcf.source = 'tmp.txt'
@@ -46,6 +47,7 @@ def _build_test_case_file():
     _add_variable_table(tcf)
     _add_keyword_table(tcf)
     return tcf
+
 
 def _add_settings_table(tcf):
     tcf.setting_table.add_library(OS_LIB)
@@ -59,6 +61,7 @@ def _add_settings_table(tcf):
     tcf.setting_table.add_resource(RESOURCE_WITH_VARIABLE_IN_PATH)
     tcf.setting_table.add_variables(INVALID_FILE_PATH)
 
+
 def _add_variable_table(tcf):
     tcf.variable_table.add(LIB_NAME_VARIABLE, COLLECTIONS_LIB)
     tcf.variable_table.add(RES_NAME_VARIABLE, RESOURCE_WITH_VARS)
@@ -67,10 +70,13 @@ def _add_variable_table(tcf):
     tcf.variable_table.add(COLLIDING_CONSTANT, 'collision')
     tcf.variable_table.add('&{dict var}', {'key': 'value'})
 
+
 def _add_keyword_table(tcf):
     uk_table = tcf.keyword_table
     uk_table.add(EXISTING_USER_KEYWORD)
-    uk_table.keywords[0].args.value = ['${keyword argument}', '${colliding argument}', '${keyword argument with default} = default']
+    uk_table.keywords[0].args.value = [
+        '${keyword argument}', '${colliding argument}',
+        '${keyword argument with default} = default']
 
 
 class ParentMock(object):
@@ -103,7 +109,9 @@ class TestKeywordSuggestions(_DataFileTest):
     def test_getting_suggestions_for_empty_datafile(self):
         start = 'shOulD'
         sugs = self.ns.get_suggestions_for(self.kw, start)
-        assert_true(len(sugs) > 0)
+        for s in sugs:
+            print s.name
+        assert_false(len(sugs) > 0)
         for s in sugs:
             assert_true(s.name.lower().startswith(start.lower()))
 
@@ -130,7 +138,8 @@ class TestKeywordSuggestions(_DataFileTest):
 
     def test_lib_import_with_spaces(self):
         sugs = self.ns.get_suggestions_for(self.kw, 'space')
-        sugs = [s for s in sugs if not isinstance(s, VariableInfo)] #remove variable suggestions
+        # remove variable suggestions
+        sugs = [s for s in sugs if not isinstance(s, VariableInfo)]
         self._assert_import_kws(sugs, 'spacelib')
 
     def test_resource_file_keywords(self):
@@ -138,7 +147,8 @@ class TestKeywordSuggestions(_DataFileTest):
         self._assert_import_kws(sugs, RESOURCES_HTML)
 
     def test_resource_file_keyword_with_longname(self):
-        sugs = self.ns.get_suggestions_for(self.kw, RESOURCES_HTML.replace('.html', '') + '.Resource Uk')
+        sugs = self.ns.get_suggestions_for(
+            self.kw, RESOURCES_HTML.replace('.html', '') + '.Resource Uk')
         self._assert_import_kws(sugs, RESOURCES_HTML)
 
     def test_keywords_normalization(self):
@@ -150,27 +160,34 @@ class TestKeywordSuggestions(_DataFileTest):
         self._assert_import_kws(sugs, 'resource.txt')
 
     def test_resource_file_from_variable(self):
-        sugs = self.ns.get_suggestions_for(self.kw, 'UK From Variable Resource')
+        sugs = self.ns.get_suggestions_for(
+            self.kw, 'UK From Variable Resource')
         self._assert_import_kws(sugs, 'resource_with_variables.txt')
 
     def test_resource_file_from_resource_file_with_variable(self):
-        sugs = self.ns.get_suggestions_for(self.kw, 'UK From Resource from Resource with Variable')
-        self._assert_import_kws(sugs, 'resource_from_resource_with_variable.txt')
+        sugs = self.ns.get_suggestions_for(
+            self.kw, 'UK From Resource from Resource with Variable')
+        self._assert_import_kws(
+            sugs, 'resource_from_resource_with_variable.txt')
 
     def test_library_from_resourcefile_variable(self):
         sugs = self.ns.get_suggestions_for(self.kw, 'Execute Manual')
         self._assert_import_kws(sugs, 'Dialogs')
 
     def test_xml_library(self):
-        sugs = self.ns.get_suggestions_for(self._get_controller(TESTCASEFILE_WITH_EVERYTHING).keywords[0], 'Attributeless Keyword')
+        sugs = self.ns.get_suggestions_for(self._get_controller(
+            TESTCASEFILE_WITH_EVERYTHING).keywords[0], 'Attributeless Keyword')
         self._assert_import_kws(sugs, 'LibSpecLibrary')
 
     def test_xml_library_is_library_keyword(self):
-        everything_tcf = TestCaseFile(source=TESTCASEFILE_WITH_EVERYTHING).populate()
-        assert_true(self.ns.is_library_keyword(everything_tcf, 'Attributeless Keyword'))
+        everything_tcf = TestCaseFile(
+            source=TESTCASEFILE_WITH_EVERYTHING).populate()
+        assert_true(self.ns.is_library_keyword(
+            everything_tcf, 'Attributeless Keyword'))
 
     def test_variable_path_separator(self):
-        sugs = self.ns.get_suggestions_for(self._get_controller(TESTCASEFILE_WITH_EVERYTHING).keywords[0], 'foo')
+        sugs = self.ns.get_suggestions_for(
+            self._get_controller(TESTCASEFILE_WITH_EVERYTHING).keywords[0], 'foo')
         self._assert_import_kws(sugs, 'even_more_resources.txt')
 
     def test_keywords_only_once_per_source(self):
@@ -192,7 +209,8 @@ class TestKeywordSuggestions(_DataFileTest):
         self._test_global_variable(global_vars[-1])
 
     def _test_global_variable(self, variable, expected=None):
-        assert_equals(expected or variable, self.ns.get_suggestions_for(self.kw, variable)[0].name)
+        assert_equals(
+            expected or variable, self.ns.get_suggestions_for(self.kw, variable)[0].name)
 
     def test_resource_with_variable_in_path(self):
         sugs = self.ns.get_suggestions_for(self.kw, 'Resu UK')
@@ -201,19 +219,22 @@ class TestKeywordSuggestions(_DataFileTest):
     def test_scalar_variable_suggestion(self):
         scalar_vars = self.ns.get_suggestions_for(self.kw, '$')
         assert_true(len(scalar_vars) > 0)
-        assert_true(len(self.ns.get_suggestions_for(self.kw, '${')) == len(scalar_vars))
+        assert_true(
+            len(self.ns.get_suggestions_for(self.kw, '${')) == len(scalar_vars))
         sug = self.ns.get_suggestions_for(self.kw, '${lib')
         assert_true(sug[0].name == LIB_NAME_VARIABLE)
 
     def test_list_variable_suggestion(self):
         list_vars = self.ns.get_suggestions_for(self.kw, '@')
         assert_true(len(list_vars) > 0)
-        assert_true(len(self.ns.get_suggestions_for(self.kw, '@{')) == len(list_vars))
+        assert_true(
+            len(self.ns.get_suggestions_for(self.kw, '@{')) == len(list_vars))
 
     def test_dict_variable_suggestion(self):
         dict_vars = self.ns.get_suggestions_for(self.kw, '&')
         assert_true(len(dict_vars) > 0)
-        assert_true(len(self.ns.get_suggestions_for(self.kw, '&{')) == len(dict_vars))
+        assert_true(
+            len(self.ns.get_suggestions_for(self.kw, '&{')) == len(dict_vars))
 
     def test_variable_suggestions_without_varwrapping(self):
         self._test_global_variable('space', '${SPACE}')
@@ -254,9 +275,11 @@ class TestKeywordSuggestions(_DataFileTest):
     def test_vars_from_keyword_arguments(self):
         sugs = self.ns.get_suggestions_for(self.kw, '${keyword argu')
         assert_equals(len(sugs), 2)
-        sugs = self.ns.get_suggestions_for(self.kw, '${keyword argument with defau')
+        sugs = self.ns.get_suggestions_for(
+            self.kw, '${keyword argument with defau')
         assert_equals(len(sugs), 1)
-        self._check_source(self.kw, '${keyword argument with defau', ArgumentInfo.SOURCE)
+        self._check_source(
+            self.kw, '${keyword argument with defau', ArgumentInfo.SOURCE)
 
     def test_argument_is_superior_to_variable_from_variable_table(self):
         sugs = self.ns.get_suggestions_for(self.kw, COLLIDING_ARGUMENT[0:4])
@@ -277,7 +300,8 @@ class TestKeywordSuggestions(_DataFileTest):
         self._check_source(everything_tcf, '${arg}', 'everything.html')
         self._check_source(everything_tcf, '@{list}', 'everything.html')
         self._check_source(everything_tcf, '${dynamic var}', 'dynamic_varz.py')
-        self._check_source(everything_tcf, '${OPERATING SYSTEM}', 'another_resource.html')
+        self._check_source(
+            everything_tcf, '${OPERATING SYSTEM}', 'another_resource.html')
 
     def test_relative_imports(self):
         relative_tcf = self._get_controller(RELATIVE_IMPORTS)
@@ -295,7 +319,7 @@ class TestKeywordSuggestions(_DataFileTest):
                         '%s does not end with %s' % (s.source, source))
 
     def test_reset(self):
-        sugs  = self.ns.get_suggestions_for(self.kw, 'generate random')
+        sugs = self.ns.get_suggestions_for(self.kw, 'generate random')
         sugs2 = self.ns.get_suggestions_for(self.kw, 'generate random')
         assert_true(sugs[0] is sugs2[0])
         self.ns.reset_resource_and_library_cache()
@@ -308,13 +332,16 @@ class TestKeywordSearch(_DataFileTest):
     def test_is_library_keyword(self):
         assert_true(self.ns.is_library_keyword(self.tcf, 'Should Be Equal'))
         assert_false(self.ns.is_library_keyword(self.tcf, 'kameli'))
-        assert_false(self.ns.is_library_keyword(self.tcf, 'UK From Resource from Resource with Variable'))
+        assert_false(self.ns.is_library_keyword(
+            self.tcf, 'UK From Resource from Resource with Variable'))
 
     def test_is_library_keyword_longname(self):
-        assert_true(self.ns.is_library_keyword(self.tcf, 'Builtin.Should Be Equal'))
+        assert_true(
+            self.ns.is_library_keyword(self.tcf, 'Builtin.Should Be Equal'))
 
     def test_is_library_keyword_longname_with_alias(self):
-        assert_true(self.ns.is_library_keyword(self.tcf, TELNET_LIB_ALIAS+'.LOGIN'))
+        assert_true(
+            self.ns.is_library_keyword(self.tcf, TELNET_LIB_ALIAS+'.LOGIN'))
 
     def test_find_default_keywords(self):
         all_kws = self.ns.get_all_keywords([])
@@ -322,7 +349,8 @@ class TestKeywordSearch(_DataFileTest):
         self.assert_in_keywords(all_kws, 'Should Be Equal')
 
     def test_find_suite_keywords(self):
-        everything_tcf = TestCaseFile(source=TESTCASEFILE_WITH_EVERYTHING).populate()
+        everything_tcf = TestCaseFile(
+            source=TESTCASEFILE_WITH_EVERYTHING).populate()
         all_kws = self.ns.get_all_keywords([self.tcf, everything_tcf])
         self.assert_in_keywords(all_kws, 'Should be in keywords Uk',
                                          'Copy List',
@@ -342,32 +370,45 @@ class TestKeywordSearch(_DataFileTest):
         self._check_resource_keyword_only_once(all_kws)
 
     def _check_resource_keyword_only_once(self, all_kws):
-        results = [(kw.name, kw.source) for kw in all_kws if kw.name == "Only From Resource"]
+        results = [(kw.name, kw.source)
+                   for kw in all_kws if kw.name == "Only From Resource"]
         assert_equals(len(results), 1)
-        assert_equals(results[0], (u'Only From Resource', u'testdata_resource.txt'))
+        assert_equals(
+            results[0], (u'Only From Resource', u'testdata_resource.txt'))
 
     def test_find_user_keyword_name_normalized(self):
-        assert_is_not_none(self.ns.find_user_keyword(self.tcf, 'UK Fromresource from rESOURCE with variaBLE'))
+        assert_is_not_none(self.ns.find_user_keyword(
+            self.tcf, 'UK Fromresource from rESOURCE with variaBLE'))
         assert_is_none(self.ns.find_user_keyword(self.tcf, 'Copy List'))
 
     def test_is_user_keyword(self):
-        assert_true(self.ns.is_user_keyword(self.tcf, 'UKFromResource from ResourcewithVariable'))
+        assert_true(self.ns.is_user_keyword(
+            self.tcf, 'UKFromResource from ResourcewithVariable'))
         assert_false(self.ns.is_user_keyword(self.tcf, 'hevoinen'))
         assert_false(self.ns.is_user_keyword(self.tcf, 'Should Be Equal'))
 
     def test_is_user_keyword_in_resource_file(self):
-        everything_tcf = TestCaseFile(source=TESTCASEFILE_WITH_EVERYTHING).populate()
-        assert_is_not_none(self.ns.find_user_keyword(everything_tcf, 'Duplicate UK'))
+        everything_tcf = TestCaseFile(
+            source=TESTCASEFILE_WITH_EVERYTHING).populate()
+        assert_is_not_none(
+            self.ns.find_user_keyword(everything_tcf, 'Duplicate UK'))
         assert_true(self.ns.is_user_keyword(everything_tcf, 'Duplicate UK'))
-        assert_is_not_none(self.ns.find_user_keyword(everything_tcf, 'Another Resource UK'))
-        assert_true(self.ns.is_user_keyword(everything_tcf, 'Another Resource UK'))
+        assert_is_not_none(
+            self.ns.find_user_keyword(everything_tcf, 'Another Resource UK'))
+        assert_true(
+            self.ns.is_user_keyword(everything_tcf, 'Another Resource UK'))
 
     def test_given_when_then_and_aliases(self):
-        assert_is_not_none(self.ns.find_user_keyword(self.tcf, '  Given   UK Fromresource from rESOURCE with variaBLE'))
-        assert_is_not_none(self.ns.find_user_keyword(self.tcf, 'when  UK Fromresource from rESOURCE with variaBLE'))
-        assert_is_not_none(self.ns.find_user_keyword(self.tcf, '  then UK Fromresource from rESOURCE with variaBLE'))
-        assert_is_not_none(self.ns.find_user_keyword(self.tcf, 'AND UK Fromresource from rESOURCE with variaBLE'))
-        assert_is_none(self.ns.find_user_keyword(self.tcf, 'given and UK Fromresource from rESOURCE with variaBLE'))
+        assert_is_not_none(self.ns.find_user_keyword(
+            self.tcf, '  Given   UK Fromresource from rESOURCE with variaBLE'))
+        assert_is_not_none(self.ns.find_user_keyword(
+            self.tcf, 'when  UK Fromresource from rESOURCE with variaBLE'))
+        assert_is_not_none(self.ns.find_user_keyword(
+            self.tcf, '  then UK Fromresource from rESOURCE with variaBLE'))
+        assert_is_not_none(self.ns.find_user_keyword(
+            self.tcf, 'AND UK Fromresource from rESOURCE with variaBLE'))
+        assert_is_none(self.ns.find_user_keyword(
+            self.tcf, 'given and UK Fromresource from rESOURCE with variaBLE'))
 
     def assert_in_keywords(self, keywords, *kw_names):
         for kw_name in kw_names:
@@ -437,7 +478,7 @@ class TestResourceGetter(_DataFileTest):
 
     def test_resource_getter(self):
         resources = self.ns.get_resources(self.tcf)
-        assert_equals(len(resources),8)
+        assert_equals(len(resources), 8)
         paths = []
         for res in resources:
             normalized = normpath(res.source)
@@ -467,8 +508,10 @@ class TestResourceCache(_DataFileTest):
     if IS_WINDOWS:
         def test_case_sensetive_filenames(self):
             imp = Resource(None, RESOURCE_PATH)
-            first = self._res_cache.get_resource(imp.directory, imp.name.lower())
-            second = self._res_cache.get_resource(imp.directory, imp.name.upper())
+            first = self._res_cache.get_resource(
+                imp.directory, imp.name.lower())
+            second = self._res_cache.get_resource(
+                imp.directory, imp.name.upper())
             assert_true(first is second)
 
 if __name__ == "__main__":
