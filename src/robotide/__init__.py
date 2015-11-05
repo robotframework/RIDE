1c2bb9bd3b4f8b208dabd0ecfd343ed9e137d042
HelioGuilherme66 <helioxentric@gmail.com>
2015-11-05 01:35:00 +0000

    Adds wxPython versions 2.9 and 3.0. Fixes Preferences/Save panel size.
    Gets RIDE running with wxPython 3.0.2.0.
    There is a known bug when consulting RIDE log (causes Segmentation fault).
    
    Debugging RIDE log.
    
    Changes log.py to create Frame instead of TxtCtrl.
    
    TEMPORARILY disables RecentFiles core plugin. This makes possible to proceed
    with other debbugging. Now IDE runs OK in Windows 7, and shows RIDE Log OK.

--- src/robotide/__init__.py
+++ src/robotide/__init__.py
@@ -32,15 +32,13 @@ import os
 from string import Template
 
 errorMessageTemplate = Template("""$reason
-You need to install wxPython 2.8.12.1 with unicode support to run RIDE.
-wxPython 2.8.12.1 can be downloaded from http://sourceforge.net/projects/wxpython/files/wxPython/2.8.12.1/""")
-supported_versions = ["2.8"]
+You need to install wxPython 2.9.1 (or newer) with unicode support to run RIDE.
+wxPython 2.9.1.1 can be downloaded from http://sourceforge.net/projects/wxpython/files/wxPython/2.9.1.1/""")
+supported_versions = ["2.9", "3.0"]
 
 try:
     import wxversion
     from wxversion import VersionError
-    if sys.platform == 'darwin':
-        supported_versions.append("2.9")
     wxversion.select(supported_versions)
     import wx
 except ImportError as e:
@@ -104,19 +102,18 @@ def _run(inpath=None, updatecheck=True, debug_console=False):
 
 
 def _show_old_wxpython_warning_if_needed(parent=None):
-    if wx.VERSION >= (2, 8, 12, 1):
+    if wx.VERSION >= (2, 9, 1, 1):
         return
     title = 'Please upgrade your wxPython installation'
-    message = ('RIDE officially supports wxPython 2.8.12.1. '
+    message = ('RIDE officially supports wxPython 2.9.1.1. '
                'Your current version is %s.\n\n'
-               'Older wxPython versions are known to miss some features used by RIDE. '
-               'Notice also that wxPython 3.0 is not yet supported.\n\n'
-               'wxPython 2.8.12.1 packages can be found from\n'
-               'http://sourceforge.net/projects/wxpython/files/wxPython/2.8.12.1/.'
+               'Older wxPython versions are no longer supported by this version of RIDE. '
+               'RIDE 1.4.1 was the last version to support wxPython 2.8.12.1.\n'
+               'Recent versions can be downloaded from http://sourceforge.net/projects/wxpython/files/wxPython/.'
                % wx.VERSION_STRING)
     style = wx.ICON_EXCLAMATION
     if not parent:
-        _ = wx.PySimpleApp()
+        _ = wx.App()
         parent = wx.Frame(None, size=(0,0))
     wx.MessageDialog(parent, message, title, style).ShowModal()
 
--- src/robotide/log/log.py
+++ src/robotide/log/log.py
@@ -18,7 +18,6 @@ import tempfile
 import uuid
 import atexit
 import glob
-import sys
 
 from robotide.pluginapi import Plugin, ActionInfo, RideLog
 from robotide import widgets
@@ -41,6 +40,7 @@ class LogPlugin(Plugin):
         self._window = None
         self._path = os.path.join(
             tempfile.gettempdir(), '{}-ride.log'.format(uuid.uuid4()))
+        print self._path
         self._outfile = None
         self._remove_old_log_files()
         atexit.register(self._close)
@@ -52,11 +52,11 @@ class LogPlugin(Plugin):
 
     def _remove_old_log_files(self):
         for fname in glob.glob(
-                os.path.join(tempfile.gettempdir(), '*-ride.log')):
+                os.path.join(tempfile.gettempdir(), '*ride.log')):
             try:
                 os.remove(fname)
-            except OSError or IOError as e:
-                sys.stderr.write("{}".format(e))
+            except IOError:
+                pass
 
     @property
     def _logfile(self):
@@ -103,33 +103,37 @@ class LogPlugin(Plugin):
             self.notebook.show_tab(self._window)
 
 
-class _LogWindow(wx.TextCtrl):
+class _LogWindow(wx.Panel):
 
     def __init__(self, notebook, log):
-        wx.TextCtrl.__init__(
-            self, notebook, style=wx.TE_READONLY | wx.TE_MULTILINE)
+        wx.Panel.__init__(self, notebook)
+        self._output = wx.TextCtrl(self, style=wx.TE_READONLY | wx.TE_MULTILINE)
         self._log = log
         self._create_ui()
         self._add_to_notebook(notebook)
         self.SetFont(widgets.Font().fixed_log)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
 
     def _create_ui(self):
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self)
-        self.SetSizer(sizer)
+        self.SetSizer(widgets.VerticalSizer())
+        self.Sizer.add_expanding(self._output)
 
     def _add_to_notebook(self, notebook):
         notebook.add_tab(self, 'Log', allow_closing=True)
         notebook.show_tab(self)
+        self._output.SetSize(self.Size)
 
     def close(self, notebook):
         notebook.delete_tab(self)
 
     def update_log(self):
-        self.SetValue(self._decode_log(self._log))
+        self._output.SetValue(self._decode_log(self._log))
 
     def _decode_log(self, log):
         result = ''
         for msg in log:
             result += _message_to_string(msg)
         return result
+
+    def OnSize(self, evt):
+        self._output.SetSize(self.Size)
