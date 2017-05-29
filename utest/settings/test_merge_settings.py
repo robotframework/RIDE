e468608d48fcbb0228cbf080f6a46aa0de05c1b2
HelioGuilherme66 <helioxentric@gmail.com>
2017-05-30 00:43:05 +0100

    Util and new(?) test. Python3 based file.

---
+++ utest/settings/test_merge_settings.py
@@ -0,0 +1,31 @@
+import os.path
+import unittest
+
+from resources.setting_utils import TestSettingsHelper
+from robotide.preferences.settings import SettingsMigrator
+
+
+class TestMergeSettings(TestSettingsHelper):
+
+    def setUp(self):
+        base = os.path.join(os.path.dirname(__file__), '..', 'resources')
+        self.settings_cfg = os.path.join(base, 'settings2.cfg')
+        self.user_cfg = os.path.join(base, 'user2.cfg')
+
+    def tearDown(self):
+        pass
+
+    def test_merge_settings(self):
+        SettingsMigrator(self.settings_cfg, self.user_cfg).merge()
+        # SettingsMigrator(self.settings_cfg, self.user_cfg).merge()
+        # SettingsMigrator(self.settings_cfg, self.settings_cfg).merge()
+        content = self._read_settings_file_content(self.user_cfg)
+        # content = self._read_settings_file_content(self.settings_cfg)
+        line_count = len(content.splitlines())
+        print("DEBUG: len %s content:\n%s" % (line_count, content))
+        # self.assertEquals(line_count, 33, "line count should be 33 was %s" %
+        #                  line_count)
+
+
+if __name__ == "__main__":
+    unittest.main()
