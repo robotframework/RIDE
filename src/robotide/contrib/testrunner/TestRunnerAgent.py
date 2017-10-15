a846aedf2a9543f797bbab0e513932bd1312eb03
HelioGuilherme66 <helioxentric@gmail.com>
2017-10-16 00:58:21 +0100

    Remove error on import libraries.

--- src/robotide/contrib/testrunner/TestRunnerAgent.py
+++ src/robotide/contrib/testrunner/TestRunnerAgent.py
@@ -68,12 +68,15 @@ except ImportError:#py3
         raise e
 
 try:
-    from robotide.lib.robot.errors import ExecutionFailed
-    from robotide.lib.robot.running import EXECUTION_CONTEXTS
-    from robotide.lib.robot.running.signalhandler import STOP_SIGNAL_MONITOR
-    from robotide.lib.robot.utils import encoding
+    # to find robot (we use provided lib)
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../../lib'))
+    from robot.errors import ExecutionFailed
+    from robot.running import EXECUTION_CONTEXTS
+    from robot.running.signalhandler import STOP_SIGNAL_MONITOR
+    from robot.utils import encoding
 except ImportError:
     encoding = None
+    # print("TestRunnerAgent: Maybe you did not installed RIDE under this Python?")  # DEBUG
     raise     # DEBUG
 
 
