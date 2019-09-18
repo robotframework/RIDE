457afbdab1a0a0add3e437693cd08db67313111a
Nyral <39315668+Nyral@users.noreply.github.com>
2019-09-18 22:30:27 +0300

    #2012 Ported next() to be compatible with both python 2 and 3 (#2013)

--- src/robotide/preferences/configobj.py
+++ src/robotide/preferences/configobj.py
@@ -201,7 +201,7 @@ class Builder(object):
         d = {}
         i = iter(map(self.build, o.getChildren()))
         for el in i:
-            d[el] = i.next()
+            d[el] = next(i)
         return d
 
     def build_Tuple(self, o):
