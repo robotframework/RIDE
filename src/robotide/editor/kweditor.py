472e71481e20e977fe612e76d8c668037646e75e
Nyral <r4nd0m4u@gmail.com>
2019-12-28 23:02:59 +0200

    Added debug prints and SelectAll shortcut when pressing Alt+A

--- src/robotide/editor/kweditor.py
+++ src/robotide/editor/kweditor.py
@@ -579,9 +579,11 @@ class KeywordEditor(with_metaclass(classmaker(), GridEditor, RideEventHandler)):
 
     def OnKeyDown(self, event):
         keycode = event.GetKeyCode()
-
+        print('DEBUG: pressed keycode', keycode)
         if event.ControlDown():
+            print('DEBUG: ControlDown pressed!')
             if event.ShiftDown():
+                print('DEBUG: ShiftDown pressed!')
                 if keycode == ord('I'):
                     self.OnInsertCells()
                 elif keycode == ord('J'):
@@ -600,6 +602,7 @@ class KeywordEditor(with_metaclass(classmaker(), GridEditor, RideEventHandler)):
                 elif keycode == ord('Z'):
                     self.OnUndo(event)
                 elif keycode == ord('A'):
+                    print('DEBUG: SelectAll pressed!')
                     self.OnSelectAll(event)
                 elif keycode == ord('B'):
                     self._navigate_to_matching_user_keyword(
@@ -614,8 +617,12 @@ class KeywordEditor(with_metaclass(classmaker(), GridEditor, RideEventHandler)):
                 else:
                     self.show_cell_information()
         elif event.AltDown():
+            print('DEBUG: AltDown pressed!', keycode)
             if keycode == wx.WXK_SPACE:
                 self._open_cell_editor_with_content_assist() # Mac CMD
+            elif keycode == ord('A'):
+                print('DEBUG: SelectAll pressed!')
+                self.OnSelectAll(event)
             elif keycode in [wx.WXK_DOWN, wx.WXK_UP]:
                 self._skip_except_on_mac(event)
                 self._move_rows(keycode)
