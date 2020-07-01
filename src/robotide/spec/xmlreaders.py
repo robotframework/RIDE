0f9733323b89c8faf10ab5ba071b190a79c5863e
HelioGuilherme66 <helioxentric@gmail.com>
2020-07-01 01:38:44 +0100

    Fix local import

--- src/robotide/spec/xmlreaders.py
+++ src/robotide/spec/xmlreaders.py
@@ -18,7 +18,7 @@ import sys
 
 from robotide import robotapi, utils
 from robotide.utils.versioncomparator import cmp_versions
-from iteminfo import _XMLKeywordContent
+from .iteminfo import _XMLKeywordContent
 from robotide import context
 
 class SpecInitializer(object):
