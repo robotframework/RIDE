e885830768c9555f0c9ada1f86e60fa1bd72693f
Johnny.H <jnhyperion@gmail.com>
2020-09-22 15:42:34 +0800

    Specify wxPython version for MacOS

--- setup.py
+++ setup.py
@@ -14,24 +14,26 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import os
 import sys
 from os.path import abspath, join, dirname
-
-sys.path.append(join(dirname(__file__), 'src'))
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 ROOT_DIR = dirname(abspath(__file__))
 SOURCE_DIR = 'src'
-REQUIREMENTS = ['wxPython', 'PyPubSub', 'Pygments', 'psutil']
-
-#Windows specific requirements
-if sys.platform == 'win32':
-    REQUIREMENTS.append('Pywin32')
+REQUIREMENTS = ['PyPubSub',
+                'Pygments',
+                'psutil',
+                'Pywin32; sys_platform=="win32"',
+                'wxPython>=4.1.0; sys_platform=="darwin"',
+                'wxPython>=4.0.7.post2; sys_platform=="linux"',
+                'wxPython>=4.0.7.post2; sys_platform=="win32"']
 
 version_file = join(ROOT_DIR, 'src', 'robotide', 'version.py')
 exec(compile(open(version_file).read(), version_file, 'exec'))
 
-package_data = {
+PACKAGE_DATA = {
     'robotide.preferences': ['settings.cfg'],
     'robotide.widgets': ['*.png', '*.gif', '*.ico'],
     'robotide.messages': ['*.html'],
@@ -40,28 +42,26 @@ package_data = {
                              'RIDE.app/Contents/MacOS/RIDE', 'RIDE.app/Contents/Resources/*.icns']
 }
 
-long_description = """
+LONG_DESCRIPTION = """
 Robot Framework is a generic test automation framework for acceptance
 level testing. RIDE is a lightweight and intuitive editor for Robot
 Framework test data.
 """.strip()
 
-classifiers = """
+CLASSIFIERS = """
 Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
-Programming Language :: Python
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Topic :: Software Development :: Testing
 """.strip().splitlines()
 
+
 # This solution is found at http://stackoverflow.com/a/26490820/5889853
-from setuptools.command.install import install
-import os
-import sys
-
-
 class CustomInstallCommand(install):
-    """Customized setuptools install command - prints a friendly greeting."""
+    """Customized setuptools install command - install RIDE desktop shortcut."""
     def run(self):
         install.run(self)
         _ = sys.stderr.write("Creating Desktop Shortcut to RIDE...\n")
@@ -69,28 +69,28 @@ class CustomInstallCommand(install):
         command = sys.executable + " " +postinstaller_file + " -install"
         os.system(command)
 
+
 setup(
     name='robotframework-ride',
     version=VERSION,
     description='RIDE :: Robot Framework Test Data Editor',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     license='Apache License 2.0',
     keywords='robotframework testing testautomation',
     platforms='any',
-    classifiers=classifiers,
+    classifiers=CLASSIFIERS,
     author='Robot Framework Developers',
     author_email='robotframework@gmail.com',
     url='https://github.com/robotframework/RIDE/',
     download_url='https://pypi.python.org/pypi/robotframework-ride',
-    install_requires = REQUIREMENTS,
+    install_requires=REQUIREMENTS,
     package_dir={'': SOURCE_DIR},
     packages=find_packages(SOURCE_DIR),
-    package_data=package_data,
+    package_data=PACKAGE_DATA,
     python_requires='>=3.6',
     # Robot Framework package data is not included, but RIDE does not need it.
     # Always install everything, since we may be switching between versions
     options={'install': {'force': True}},
     scripts=['src/bin/ride.py', 'src/bin/ride_postinstall.py'],
     cmdclass={'install': CustomInstallCommand},
-    requires=['Pygments', 'wxPython', 'PyPubSub']
 )
