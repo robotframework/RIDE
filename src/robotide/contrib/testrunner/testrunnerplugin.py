8ad9886c4bbb8b98469583343b6c06539dd14043
Junling Zhu <jl.zhu@tom.com>
2022-10-09 14:29:21 +0800

    fix bug #2361 Chinese name of test case can not display correct on console log

--- src/robotide/contrib/testrunner/testrunnerplugin.py
+++ src/robotide/contrib/testrunner/testrunnerplugin.py
@@ -602,6 +602,8 @@ class TestRunnerPlugin(Plugin):
 
     def _append_to_console_log(self, text, source="stdout"):
         """Put output to the text control"""
+        if isinstance(text, (bytes, bytearray)):
+            text = text.decode(encoding['CONSOLE'], 'backslashreplace')
         self._append_text(self._console_log_ctrl, text, source)
         if self._console_log:
             FileWriter.write(self._console_log, [text], "ab", "a")
