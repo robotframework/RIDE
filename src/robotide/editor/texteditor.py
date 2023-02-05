c2ae776e7f537b4f580afe042b4898e23cb13271
Harm <harm.kramer@gmail.com>
2023-02-05 18:28:06 +0100

    Add Ctrl-D to Text Editor (#2523)
    * CTRL-D + author surname
    
    * Fix uncomment in Text Editor (#2529)
    
    ** Fix comments in steps with arguments
    
    * Improve uncomment in Text Editor
    
    * Fix comments (#2530)
    
    * Update appveyor python versions
    
    * Experience with appveyor configuration
    
    * Experience with appveyor configuration 2
    
    * Experience with appveyor configuration 3
    
    * Experience with appveyor configuration 4
    
    * Experience with appveyor configuration 5
    
    * Update appveyor python versions
    
    * WIP: compute args in Step.as_list()
    
    * Fix comments in steps with arguments
    
    * Fix problems in Grid Editor on keywords with steps  with #comments
    
    * Revert version number
    
    * Note about updating from current development 2.0rc1
    
    * Devops (#2531)
    
    * Update appveyor python versions
    
    * Try to test in macos
    
    * Change to allow building in this branch
    
    * Use python3.9
    
    * Install python3.9 and try to get information about system
    
    * Install pytest
    
    * Change to use invoke test
    
    * Update .appveyor.yml
    
    commented path definition
    
    * Texteditor ctrl i (#2522)
    
    * Add Ctrl-I to Text Editor
    
    * Insert multiselected rows
    
    * Improve multiline insert and cursor position
    
    ---------
    
    Co-authored-by: HelioGuilherme66 <helioxentric@gmail.com>
    
    * Improve Ctrl-D function in Text Editor
    
    * Update CHANGELOG
    
    ---------
    
    Co-authored-by: Hélio Guilherme <helioxentric@gmail.com>

--- src/robotide/editor/texteditor.py
+++ src/robotide/editor/texteditor.py
@@ -85,6 +85,7 @@ class TextEditorPlugin(Plugin, TreeAwarePluginMixin):
         self.register_shortcut('CtrlCmd-Z', focused(lambda e: self._editor.undo()))
         self.register_shortcut('CtrlCmd-Y', focused(lambda e: self._editor.redo()))
         # self.register_shortcut('Del', focused(lambda e: self._editor.delete()))
+        # self.register_shortcut('CtrlCmd-D', focused(lambda e: self._editor.delete_row(e)))
         self.register_shortcut('CtrlCmd-I', focused(lambda e: self._editor.insert_row(e)))
         self.register_shortcut('CtrlCmd-3', focused(lambda e: self._editor.execute_comment(e)))
         self.register_shortcut('CtrlCmd-Shift-3', focused(lambda e: self._editor.execute_sharp_comment(e)))
@@ -925,6 +926,8 @@ class SourceEditor(wx.Panel):
             self.execute_variable_creator(list_variable=(keycode == ord('2')),
                                           dict_variable=(keycode == ord('5')))
             self.store_position()
+        elif keycode == ord('D') and event.ControlDown():
+            self.delete_row(event)
         else:
             event.Skip()
         """
@@ -998,6 +1001,29 @@ class SourceEditor(wx.Panel):
         else:
             close_symbol = open_symbol
         return open_symbol+value+close_symbol
+
+    def delete_row(self, event):
+        start, end = self._editor.GetSelection()
+        cursor = self._editor.GetCurrentPos()
+        ini_line = self._editor.LineFromPosition(start)
+        end_line = self._editor.LineFromPosition(end)
+        begpos = self._editor.PositionFromLine(ini_line)
+        self._editor.SelectNone()
+        # print(f"DEBUG: delete_row Variables: select start={start}, end={end} cursor={cursor}"
+        #       f" ini_line={ini_line} end_line={end_line} begpos={begpos} endpos={endpos}")
+        if start == end:
+            end_line = ini_line
+        for line in range(ini_line, end_line + 1):
+            self._editor.GotoLine(ini_line)
+            self._editor.LineDelete()
+        # cursor position when doing block select is always the end of the selection
+        if ini_line != end_line:
+            self._editor.SetCurrentPos(begpos)
+            self._editor.SetAnchor(begpos)
+        else:
+            self._editor.SetCurrentPos(cursor)
+            self._editor.SetAnchor(cursor)
+        self.store_position()
 
     def insert_row(self, event):
         start, end = self._editor.GetSelection()
