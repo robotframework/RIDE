1cbbb8ef0582d2a8ca60deeaf719ec7da13781d3
HelioGuilherme66 <helioxentric@gmail.com>
2023-02-26 02:38:51 +0000

    Update tool and release notes. Add missing RETURN doc

--- tools/mkhtml.py
+++ tools/mkhtml.py
@@ -1,8 +1,34 @@
-from robotide.version import VERSION
+import sys
+from os.path import exists
+from subprocess import run
 import docutils.core
+# Requires sudo dnf install pandoc and pip install pypandoc
+import pypandoc
+
+from pathlib import Path
+assert Path.cwd().resolve() == Path(__file__).resolve().parent
+
+sys.path.insert(0, '../src')
+from robotide.version import VERSION
+from robotide.application.releasenotes import RELEASE_NOTES
+source_path = "../doc/releasenotes/ride-"+".".join([str(x) for x in VERSION.split('.')[:2]]).replace('v','')+".rst"
+
+# Reverted flow: First edit ../src/robotide/application/releasenotes.py and then generate ride-VERSION.rst
+if exists(source_path):
+    print(f"Release notes exists: {source_path}")
+else:
+    destination_path = source_path.replace(".rst", ".html")
+    with open(destination_path, "w") as rn:
+        rn.write("<HTML>\n<BODY>\n\n")
+        rn.write(f"{RELEASE_NOTES}\n")
+        rn.write("\n</BODY>\n</HTML>\n")
+    # With an input file: it will infer the input format from the filename
+    output = pypandoc.convert_file(destination_path, 'rst', format='html')
+    with open(source_path, "w") as rst:
+        rst.write(output)
 
 docutils.core.publish_file(
-    source_path="../doc/releasenotes/ride-"+".".join([str(x) for x in VERSION.split('.')[:2]]).replace('v','')+".rst",
+    source_path=source_path,
     destination_path="../src/robotide/application/release_notes.html",
     writer_name="html")
 
@@ -13,8 +39,6 @@ print("Now paste content of ../src/robotide/application/release_notes.html to"
 source_path = "../CHANGELOG.adoc"
 directory = "../src/robotide/application"
 destination_path = directory + "/CHANGELOG.html"
-
-from subprocess import run
 
 run(["a2x3", "-f", "xhtml", "-D", directory, source_path])
 
