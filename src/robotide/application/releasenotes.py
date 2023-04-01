5ab6d3bac5032e890a8d025d1a63c23e537e7733
Hélio Guilherme <helioxentric@gmail.com>
2023-04-01 22:22:32 +0100

    Improves color management in Panels and Help/Doc popups (#2566)
    * Fix using defined color in help and HTML content
    
    * Fix persistance of color settings in File Explorer and Project Tree

--- src/robotide/application/releasenotes.py
+++ src/robotide/application/releasenotes.py
@@ -14,6 +14,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import os
+import re
+import time
 import wx
 from wx import Colour
 from wx.lib.ClickableHtmlWindow import PyClickableHtmlWindow
@@ -22,7 +25,8 @@ from os.path import abspath, join, dirname
 from ..action import ActionInfo
 from ..version import VERSION
 from ..widgets import HtmlDialog
-# from ..widgets.htmlwnd import HTML_BACKGROUND
+
+HTML_FOREGROUND = 'foreground text'
 
 
 class ReleaseNotes(object):
@@ -57,19 +61,30 @@ class ReleaseNotes(object):
             self.application.settings['version_shown'] = VERSION
 
     def show(self, event=None):
+        _ = event
         if not self._view:
             self._view = self._create_view()
             self.application.frame.notebook.AddPage(self._view, "Release Notes", select=False)
         self.application.frame.notebook.show_tab(self._view)
 
     def show_changelog(self, event=None):
+        _ = event
         if not self._dialog:
             self._dialog = HtmlDialog('Offline Change Log', f"Check the online version at https://github.com/"
                                                             f"robotframework/RIDE/blob/v{VERSION}/CHANGELOG.adoc")
         self._dialog.SetSize(800, 800)
-        self._dialog.html_wnd.LoadFile(join(dirname(abspath(__file__)), "CHANGELOG.html"))
+        # DEBUG: If we LoadFile, we cannot change the foreground color
+        # self._dialog.html_wnd.LoadFile(join(dirname(abspath(__file__)), "CHANGELOG.html"))
+        with open(join(dirname(abspath(__file__)), "CHANGELOG.html"), 'r', encoding='utf-8') as change_log:
+            content = change_log.read()
+        fgcolor = self.general_settings[HTML_FOREGROUND]
+        if isinstance(fgcolor, tuple):
+            fgcolor = '#' + ''.join(hex(item)[2:] for item in fgcolor)
+        new_content = content.replace("<body>", f'<body><div><font color="{fgcolor}">') \
+            .replace("</body>", "</font></div></body>")
+        self._dialog.html_wnd.SetPage(new_content)
         self._dialog.html_wnd.SetBackgroundColour(self.general_settings['background help'])
-        self._dialog.html_wnd.SetForegroundColour(self.general_settings['foreground text'])
+        self._dialog.html_wnd.SetForegroundColour(fgcolor)
         self._dialog.Show()
 
     def bring_to_front(self):
@@ -80,34 +95,25 @@ class ReleaseNotes(object):
         panel = wx.Panel(self.application.frame.notebook)
         html_win = PyClickableHtmlWindow(panel, -1)
         html_win.SetStandardFonts()
-        fgcolor = self.general_settings.get('foreground text', Colour(7, 0, 70))
-        """
-        panel.SetBackgroundColour(Colour(200, 222, 40))
-        """
+        fgcolor = self.general_settings.get(HTML_FOREGROUND, Colour(7, 0, 70))
         panel.SetForegroundColour(fgcolor)
         html_win.SetOwnForegroundColour(fgcolor)
         self.set_content(html_win, WELCOME_TEXT + RELEASE_NOTES)
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(html_win, 1, wx.EXPAND|wx.ALL, border=8)
+        sizer.Add(html_win, 1, wx.EXPAND | wx.ALL, border=8)
         panel.SetSizer(sizer)
         return panel
 
     def set_content(self, html_win, content):
         bkgcolor = self.general_settings.get('background help', Colour(240, 242, 80))
-        fgcolor = self.general_settings.get('foreground text', Colour(7, 0, 70))
-        # print(f"DEBUG: set_content  bkg={bkgcolor} bkg={type(bkgcolor)} fg={fgcolor} fg={type(fgcolor)}")
-        try:
-            # tuple(bkgcolor)
-            bcolor = ''.join(hex(item)[2:] for item in bkgcolor)
-            fcolor = ''.join(hex(item)[2:] for item in fgcolor)
-            _content = '<body "bgcolor=#%s;" "color=#%s;">%s</body>' % (bcolor, fcolor, content)
-            # print(f"DEBUG: set_content after  bkg={bcolor} bkg={type(bcolor)} fg={fcolor} fg={type(fcolor)}")
-        except TypeError:
-            _content = '<body bgcolor=%s>%s</body>' % (bkgcolor, content)
+        fgcolor = self.general_settings.get(HTML_FOREGROUND, Colour(7, 0, 70))
+        if isinstance(bkgcolor, tuple):
+            bkgcolor = '#' + ''.join(hex(item)[2:] for item in bkgcolor)
+        if isinstance(fgcolor, tuple):
+            fgcolor = '#' + ''.join(hex(item)[2:] for item in fgcolor)
+        _content = f'<body bgcolor="{bkgcolor}"><div><font color="{fgcolor}">' + content + "</font></div></body>"
         html_win.SetPage(_content)
 
-
-import time, os, re
 
 date = time.strftime('%d/%m/%Y', time.localtime(os.path.getmtime(__file__)))
 version = VERSION
@@ -138,8 +144,11 @@ RELEASE_NOTES = f"""
 <div class="document">
 
 
-<p><a class="reference external" href="https://github.com/robotframework/RIDE/">RIDE (Robot Framework IDE)</a> v{VERSION} is a new release with major enhancements and bug fixes.
-This version v{VERSION} includes removal of Python 2.7 support. The reference for valid arguments is <a class="reference external" href="http://robotframework.org">Robot Framework</a> installed version, which is at this moment 6.0.2. However, internal library is based on version 3.1.2, to keep compatibility with old formats.</p>
+<p><a class="reference external" href="https://github.com/robotframework/RIDE/">RIDE (Robot Framework IDE)</a>
+ v{VERSION} is a new release with major enhancements and bug fixes.
+This version v{VERSION} includes removal of Python 2.7 support. The reference for valid arguments is
+ <a class="reference external" href="http://robotframework.org">Robot Framework</a> installed version, which is at this
+  moment 6.0.2. However, internal library is based on version 3.1.2, to keep compatibility with old formats.</p>
 <p></p>
 <ul class="simple">
 <li>This is the <strong>first version without support for Python 2.7</strong>.</li>
@@ -148,13 +157,16 @@ This version v{VERSION} includes removal of Python 2.7 support. The reference fo
 <li>There are some important changes, or known issues:<ul>
 <li>On MacOS to call autocomplete in Grid and Text Editors, you have to use Alt-Space (not Command-Space).</li>
 <li>On Linux and Windows to call autocomplete in Grid and Text Editors, you have to use Ctrl-Space.</li>
-<li>On Text Editor the TAB key adds the defined number of spaces. With Shift moves to the left, and together with Control selects text.</li>
-<li>Text Editor also accepts the old <strong>: FOR</strong> loop structure, but recommended is <strong>FOR</strong> and <strong>END</strong>.</li>
+<li>On Text Editor the TAB key adds the defined number of spaces. With Shift moves to the left, and together with
+ Control selects text.</li>
+<li>Text Editor also accepts the old <strong>: FOR</strong> loop structure, but recommended is <strong>FOR</strong>
+ and <strong>END</strong>.</li>
 <li>On Grid Editor and Linux the auto enclose is only working on cell selection, but not on cell content edit.</li>
 <li>On Text Editor when Saving the selection of tests in Test Suites (Tree) is cleared.</li>
 <li>Test Suite with <em>*** Comments ***</em> can be edited but newlines are introduced.</li>
 <li>Some argument types detection (and colorization) is not correct in Grid Editor.</li>
-<li>RIDE <strong>DOES NOT KEEP</strong> Test Suites formatting or structure, causing differences in files when used on other IDE or Editors.</li>
+<li>RIDE <strong>DOES NOT KEEP</strong> Test Suites formatting or structure, causing differences in files when used
+ on other IDE or Editors.</li>
 </ul>
 </li>
 </ul>
@@ -172,23 +184,33 @@ This version v{VERSION} includes removal of Python 2.7 support. The reference fo
 </ol>
 </li>
 <li>Persistence of the position and state of detached panels, File Explorer and Test Suites</li>
-<li>File Explorer and Test Suites panels are now Plugins and can be disabled or enabled and made Visible with F11 ( Test Suites with F12, but disabled for now)</li>
+<li>File Explorer and Test Suites panels are now Plugins and can be disabled or enabled and made Visible with F11 (
+ Test Suites with F12, but disabled for now)</li>
 <li>File Explorer now shows selected file when RIDE starts</li>
 <li>Block comment and uncomment on both Grid and Text editors</li>
 <li>Extensive color customization of panel elements via <cite>Tools&gt;Preferences</cite></li>
 <li>Color use on Console and Messages Log panels on Test Run tab</li>
-<li>In Text Editor the same commands as in Grid Editor are now supported: Move Up/Down Rows, Insert or Delete Rows and Insert or Delete 'Cells'</li>
+<li>In Text Editor the same commands as in Grid Editor are now supported: Move Up/Down Rows, Insert or Delete Rows and
+ Insert or Delete 'Cells'</li>
 </ul>
 <p>We hope to implement or complete features and make fixes on next version 2.1 (in the end of 2023).</p>
 <p><strong>The minimal wxPython version is, 4.0.7, and RIDE supports the current version, 4.2.0.</strong></p>
-<p><em>Linux users are advised to install first wxPython from .whl package at</em> <a class="reference external" href="https://extras.wxpython.org/wxPython4/extras/linux/gtk3/">wxPython.org</a>, or by using the system package manager.</p>
-<p>The <a class="reference external" href="https://github.com/robotframework/RIDE/blob/master/CHANGELOG.adoc">CHANGELOG.adoc</a> lists the changes done on the different versions.</p>
+<p><em>Linux users are advised to install first wxPython from .whl package at</em> <a class="reference external"
+ href="https://extras.wxpython.org/wxPython4/extras/linux/gtk3/">wxPython.org</a>, or by using the system package
+  manager.</p>
+<p>The <a class="reference external" href="https://github.com/robotframework/RIDE/blob/master/CHANGELOG.adoc">
+CHANGELOG.adoc</a> lists the changes done on the different versions.</p>
 <p>All issues targeted for RIDE v2.0 can be found
-from the <a class="reference external" href="https://github.com/robotframework/RIDE/issues?q=milestone%3Av2.0">issue tracker milestone</a>.</p>
+from the <a class="reference external" href="https://github.com/robotframework/RIDE/issues?q=milestone%3Av2.0">issue
+ tracker milestone</a>.</p>
 <p>Questions and comments related to the release can be sent to the
-<a class="reference external" href="http://groups.google.com/group/robotframework-users">robotframework-users</a> mailing list or to the channel #ride on
-<a class="reference external" href="https://robotframework-slack-invite.herokuapp.com">Robot Framework Slack</a>, and possible bugs submitted to the <a class="reference external" href="https://github.com/robotframework/RIDE/issues">issue tracker</a>.
-You should see <a class="reference external" href="https://forum.robotframework.org/c/tools/ride/">Robot Framework Forum</a> if your problem is already known.</p>
+<a class="reference external" href="http://groups.google.com/group/robotframework-users">robotframework-users</a>
+ mailing list or to the channel #ride on
+<a class="reference external" href="https://robotframework-slack-invite.herokuapp.com">Robot Framework Slack</a>,
+ and possible bugs submitted to the <a class="reference external" href="https://github.com/robotframework/RIDE/issues">
+ issue tracker</a>.
+You should see <a class="reference external" href="https://forum.robotframework.org/c/tools/ride/">Robot Framework
+ Forum</a> if your problem is already known.</p>
 <p>To install with <a class="reference external" href="http://pip-installer.org">pip</a> installed, just run</p>
 <pre class="literal-block">
 pip install --upgrade robotframework-ride=={VERSION}
@@ -199,14 +221,19 @@ pip install --upgrade robotframework-ride
 </pre>
 
 <p>Alternatively you can download the source
-distribution from <a class="reference external" href="https://pypi.python.org/pypi/robotframework-ride">PyPI</a> and install it manually. For more details and other
-installation approaches, see the <a class="reference external" href="https://github.com/robotframework/RIDE/wiki/Installation-Instructions">installation instructions</a>.
-If you want to help in the development of RIDE, by reporting issues in current development version, you can install with:</p>
+distribution from <a class="reference external" href="https://pypi.python.org/pypi/robotframework-ride">PyPI</a> and
+ install it manually. For more details and other
+installation approaches, see the <a class="reference external"
+ href="https://github.com/robotframework/RIDE/wiki/Installation-Instructions">installation instructions</a>.
+If you want to help in the development of RIDE, by reporting issues in current development version, you can install
+ with:</p>
 <pre class="literal-block">
 pip install -U https://github.com/robotframework/RIDE/archive/master.zip
 </pre>
-<p>Important document for helping with development is the <a class="reference external" href="https://github.com/robotframework/RIDE/blob/master/CONTRIBUTING.adoc">CONTRIBUTING.adoc</a>.</p>
-<p>See the <a class="reference external" href="https://github.com/robotframework/RIDE/wiki/F.A.Q.">FAQ</a> for important info about <cite>: FOR</cite> changes and other known issues and workarounds.</p>
+<p>Important document for helping with development is the <a class="reference external"
+ href="https://github.com/robotframework/RIDE/blob/master/CONTRIBUTING.adoc">CONTRIBUTING.adoc</a>.</p>
+<p>See the <a class="reference external" href="https://github.com/robotframework/RIDE/wiki/F.A.Q.">FAQ</a> for
+ important info about <cite>: FOR</cite> changes and other known issues and workarounds.</p>
 <p>A possible way to start RIDE is:</p>
 <pre class="literal-block">
 python -m robotide.__init__
