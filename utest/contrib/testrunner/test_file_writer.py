8eb97963c13c12f7a766951f4ec2170524a8cb8d
HelioGuilherme66 <helioxentric@gmail.com>
2023-05-05 23:23:46 +0100

    Increase tests for FileWriter

--- utest/contrib/testrunner/test_file_writer.py
+++ utest/contrib/testrunner/test_file_writer.py
@@ -33,7 +33,48 @@ class FileWriterTests(unittest.TestCase):
         FileWriter.write(temp_file, args, 'wb')
         with open(temp_file, 'r') as file_reader:
             lines = file_reader.readlines()
-        os.remove(temp_file)
+        try:
+            os.remove(temp_file)
+        except PermissionError:
+            pass
+        content = [s.strip() for s in lines]
+        self.assertListEqual(content, args)
+
+    def test_file_writer_new_file(self):
+        temp_file = "/tmp/a/b/new_file.txt"
+        args = ['-C', 'off',
+                '-W', '7',
+                '-P', '/Python Dir:/opt/testlibs',
+                '-d', 'C:\\Work\\Test 1',
+                '--suite', 'suite_name_1',
+                '--test', 'test_1']
+        FileWriter.write(temp_file, args, 'wb')
+        with open(temp_file, 'r') as file_reader:
+            lines = file_reader.readlines()
+        try:
+            os.remove(temp_file)
+        except PermissionError:
+            pass
+        content = [s.strip() for s in lines]
+        self.assertListEqual(content, args)
+
+    def test_file_writer_bytes(self):
+        tmp_dir = tempfile.mkdtemp()
+        temp_file = tempfile.mkstemp(dir=tmp_dir)
+        temp_file = os.path.join(tmp_dir, temp_file[1])
+        args = [b'-C', b'off',
+                b'-W', b'7',
+                b'-P', b'/Python Dir:/opt/testlibs',
+                b'-d', b'C:\\Work\\Test 1',
+                b'--suite', b'suite_name_1',
+                b'--test', b'test_1']
+        FileWriter.write(temp_file, args, 'wb', 'wb')
+        with open(temp_file, 'rb') as file_reader:
+            lines = file_reader.readlines()
+        try:
+            os.remove(temp_file)
+        except PermissionError:
+            pass
         content = [s.strip() for s in lines]
         self.assertListEqual(content, args)
 
