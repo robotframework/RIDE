131cb32abae282646d7d7925f8e640dd9ff4fc95
HelioGuilherme66 <helioxentric@gmail.com>
2023-06-01 04:46:26 +0100

    Increases tests. One test fails with invoke but not with PyCharm

--- src/robotide/editor/gridbase.py
+++ src/robotide/editor/gridbase.py
@@ -13,6 +13,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import re
+from os import linesep
+
 import wx
 from wx import grid, Colour
 
