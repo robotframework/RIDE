72ceebd0a7701eaf51c58228fe9befc5afedf1b4
HelioGuilherme66 <helioxentric@gmail.com>
2023-06-04 13:24:31 +0100

    Remove debug info

--- utest/application/test_app_main.py
+++ utest/application/test_app_main.py
@@ -71,7 +71,7 @@ class TestMain(unittest.TestCase):
             with pytest.raises((ImportError, SystemExit)):
                 builtins.__import__ = myimport
                 result = robotide.main('--version')  # Need to capture output
-                assert result.startswith('v2.0')
+                assert result.startswith('v2.0.6')
 
     def test_parse_args(self):
         from robotide import _parse_args
