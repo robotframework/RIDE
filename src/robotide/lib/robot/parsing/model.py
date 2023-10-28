9b401aa8b05d873ddfe8c07b613e3cb129da17bf
HelioGuilherme66 <helioxentric@gmail.com>
2023-10-28 17:36:22 +0100

    Improve reader.

--- src/robotide/lib/robot/parsing/model.py
+++ src/robotide/lib/robot/parsing/model.py
@@ -524,7 +524,6 @@ class VariableTable(_Table):
         # print(f"DEBUG: RFLib model.py VariableTable add {name=}, {value=}, {comment=}")
         if not name:
             return
-        value = [x for x in value if x != '']
         self.variables.append(Variable(self, name, value, comment))
 
     def __iter__(self):
