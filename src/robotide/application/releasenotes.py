aa5b88f595df6de0d9c5e56f7e196ce921423670
HelioGuilherme66 <helioxentric@gmail.com>
2025-02-17 00:03:52 +0000

    Create directory in New Project dialog, if needed.

--- src/robotide/application/releasenotes.py
+++ src/robotide/application/releasenotes.py
@@ -168,6 +168,7 @@ version 4.2.1 and 4.2.2, which we now <em>recommend</em>.</li>
 </ul>
 <p><strong>New Features and Fixes Highlights</strong></p>
 <ul class="simple">
+<li>Create directories when needed in New Project dialog.</li>
 <li>Improved the recognition of BDD/Gherkin prefixes when localized in autocomplete on Grid Editor.</li>
 <li>Added syntax colorization for the <em>GROUP</em> marker. Improved colorization for multiple Gherkin words, for 
 example in the French language.</li>
@@ -228,7 +229,7 @@ python -m robotide
 <pre class="literal-block">python -m robotide.postinstall -install</pre>
 <p>or</p>
 <pre class="literal-block">ride_postinstall.py -install</pre>
-<p>RIDE {VERSION} was released on 16/February/2025.</p>
+<p>RIDE {VERSION} was released on 17/February/2025.</p>
 <!-- <br/>
 <h3>May The Fourth Be With You!</h3>
 <h3>Celebrate the bank holiday, 10th June, Day of Portugal, Portuguese Communities and Camões!!</h3>
