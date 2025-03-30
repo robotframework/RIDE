983bddeb5e52887de81ea2cb38f1509b0c408f9f
HelioGuilherme66 <helioxentric@gmail.com>
2025-03-30 14:36:56 +0100

    Fix language detection error

--- src/robotide/namespace/namespace.py
+++ src/robotide/namespace/namespace.py
@@ -649,9 +649,9 @@ class _Keywords(object):
                 try:
                     self.new_lang = Language.from_name(set_lang[0].replace('_','-'))
                 except ValueError:
-                    self.new_lang = new_lang
+                    self.new_lang = Language.from_name(new_lang[0])
             else:
-                self.new_lang = new_lang
+                self.new_lang = Language.from_name(new_lang[0])
         self.normalized_bdd_prefixes = utils.normalize_pipe_list(list(self.new_lang.bdd_prefixes), spaces=False)
         self.gherkin_prefix = re.compile(fr'^({self.normalized_bdd_prefixes}) (.*)', re.IGNORECASE)
         self.keywords = robotapi.NormalizedDict(ignore=['_'], caseless=caseless)
