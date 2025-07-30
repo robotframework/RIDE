d0cb9b8c1aa7c23343e03ed66ee9d605639b19d8
HelioGuilherme66 <helioxentric@gmail.com>
2025-07-30 20:11:36 +0100

    Comment unused code in texteditor

--- src/robotide/editor/texteditor.py
+++ src/robotide/editor/texteditor.py
@@ -534,12 +534,14 @@ class TextEditorPlugin(Plugin, TreeAwarePluginMixin):
             pass
 
     def _on_timer(self, event):
+        """
         if not self.is_focused():  # Was reaching here with contentassist in kweditor
             # event.Skip()
             # return
             # DEBUG: This is important to have changes done from kweditor reflected in texteditor
             # for example, a bad library import deleted from kweditor would remain in texteditor
-            print(f"DEBUG: textedit in loop _on_timer event={event}")
+            # print(f"DEBUG: textedit in loop _on_timer event={event}")
+        """
         self._editor.store_position()
         self._open_tree_selection_in_editor()
         event.Skip()
